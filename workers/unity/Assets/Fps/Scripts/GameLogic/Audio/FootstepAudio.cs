2f8f92434905a58adb075780ab4e5ea97bffa83d
Amanda Koh <amandakoh@improbable.io>
2018-10-08 14:16:27 +0100

    WORKING: added ambient sound to all map prefabs, adjust footstep sounds to stop when not moving

--- workers/unity/Assets/Fps/Scripts/GameLogic/Audio/FootstepAudio.cs
+++ workers/unity/Assets/Fps/Scripts/GameLogic/Audio/FootstepAudio.cs
@@ -16,7 +16,7 @@ namespace Fps.GameLogic.Audio
         private bool previousFootGroundedLeft;
         private bool previousFootGroundedRight;
 
-        private readonly float hitRayLength = 0.1f; 
+        private readonly float hitRayLength = 0.1f;
 
         private RaycastHit[] results = new RaycastHit[1];
 
@@ -37,25 +37,31 @@ namespace Fps.GameLogic.Audio
             var currentFootGroundedLeft = IsFootGrounded(LeftFoot);
             var currentFootGroundedRight = IsFootGrounded(RightFoot);
 
-            if (!previousFootGroundedLeft && currentFootGroundedLeft)
+            if (!previousFootGroundedLeft && currentFootGroundedLeft && NavKeysDown())
             {
                 // Left foot grounded this frame.
                 leftAudio.Play();
             }
 
-            if (!previousFootGroundedRight && currentFootGroundedRight)
+            if (!previousFootGroundedRight && currentFootGroundedRight && NavKeysDown())
             {
                 // Right foot grounded this frame.
                 rightAudio.Play();
             }
 
             previousFootGroundedLeft = currentFootGroundedLeft;
-            previousFootGroundedRight = currentFootGroundedRight; 
+            previousFootGroundedRight = currentFootGroundedRight;
         }
 
         private bool IsFootGrounded(Transform foot)
         {
             return Physics.RaycastNonAlloc(foot.position, Vector3.down, results, hitRayLength) == 1;
+        }
+
+        private bool NavKeysDown()
+        {
+            return Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) ||
+                Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D);
         }
     }
 }
