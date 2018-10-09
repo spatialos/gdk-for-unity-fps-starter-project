65f4efa62660d2a71aab8a413809d814ca733871
Firtina Ozbalikci <31209570+firtina-improbable@users.noreply.github.com>
2018-10-09 19:58:48 +0100

    Shell script improvements (#17)
    * Shell script improvements
    
    - Make shell.sh use the chosen option
    - Add notice of what the script is going to do as soon as possible
    - Preview URIs for HTTPS and SSH options
    
    * shellchecked.net
    
    * /s/\$(\w+)/${$1}
    
    I'm having way too much fun with regular expressions

--- scripts/shell/setup.sh
+++ scripts/shell/setup.sh
@@ -19,38 +19,15 @@ echo ""
 echo "Welcome to the SpatialOS GDK for Unity FPS Starter Project setup script." 
 echo ""
 
-PS3="Please make a selection: "
-echo "Select a cloning method."
-echo ""
-options=("HTTPS" "SSH" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "HTTPS")
-            CLONE_URI="https://github.com/spatialos/gdk-for-unity.git"
-            break
-            ;;
-        "SSH")
-            CLONE_URI="git@github.com:spatialos/gdk-for-unity.git"
-            break
-            ;;
-        "Quit")
-            echo "Stopping the setup process."
-            exit 0
-            ;;
-        *) echo "Invalid option $REPLY";;
-    esac
-done
-
-readonly RAW_DIR="$(dirname "$0")/../../../gdk-for-unity"
+readonly RAW_DIR="$(dirname "${0}")/../../../gdk-for-unity"
 readonly TARGET_DIRECTORY="$(realpath "${RAW_DIR}")"
 
-echo_with_color "This script will create the following directory: ${TARGET_DIRECTORY}" $LOG_WARNING
-echo_with_color "*** If such a directory already exists it will be deleted. ***" $LOG_EMPHASIS
-echo_with_color "Are you ready to proceed? (y/n)" $LOG_WARNING
+echo_with_color "This script will create the following directory: ${TARGET_DIRECTORY}" "${LOG_WARNING}"
+echo_with_color "*** If such a directory already exists it will be deleted. ***" "${LOG_EMPHASIS}"
+echo_with_color "Are you ready to proceed? (y/n)" "${LOG_WARNING}"
 
 read -r -p "" response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+if [[ "${response}" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
     echo "" > /dev/null
 else
@@ -58,18 +35,46 @@ else
     exit 0
 fi
 
+HTTPS_URI="https://github.com/spatialos/gdk-for-unity.git"
+SSH_URI="git@github.com:spatialos/gdk-for-unity.git"
+
+PS3="Please make a selection: "
+echo "Select a cloning method."
+echo ""
+HTTPS_OPTION="HTTPS (${HTTPS_URI})"
+SSH_OPTION="SSH   (${SSH_URI})"
+options=("${HTTPS_OPTION}" "${SSH_OPTION}" "Quit")
+select opt in "${options[@]}"
+do
+    case ${opt} in
+        "${HTTPS_OPTION}")
+            CLONE_URI=${HTTPS_URI}
+            break
+            ;;
+        "${SSH_OPTION}")
+            CLONE_URI=${SSH_URI}
+            break
+            ;;
+        "Quit")
+            echo "Stopping the setup process."
+            exit 0
+            ;;
+        *) echo "Invalid option ${REPLY}";;
+    esac
+done
+
 if [[ -d "${TARGET_DIRECTORY}" ]]; then
-    echo_with_color "Deleting existing directory at ${TARGET_DIRECTORY}..." $LOG_WARNING
+    echo_with_color "Deleting existing directory at ${TARGET_DIRECTORY}..." "${LOG_WARNING}"
     rm -rf "${TARGET_DIRECTORY}"
 fi
 
 echo "Cloning SpatialOS GDK for Unity"
-git clone "https://github.com/spatialos/gdk-for-unity.git" "${TARGET_DIRECTORY}"
+git clone ${CLONE_URI} "${TARGET_DIRECTORY}"
 
-PINNED_VERSION=$(cat "$(dirname "$0")/../../gdk.pinned")
+PINNED_VERSION=$(cat "$(dirname "${0}")/../../gdk.pinned")
 
 pushd "${TARGET_DIRECTORY}"
     git checkout "${PINNED_VERSION}"
 popd
 
-echo_with_color "Finished!" $LOG_SUCCESS
+echo_with_color "Finished!" "${LOG_SUCCESS}"
