8a009e677c0a98a3a59aa7ff1c7f6ba0e951b45b
Firtina O <firtina@improbable.io>
2018-10-09 19:06:12 +0100

    /s/\$(\w+)/${$1}
    I'm having way too much fun with regular expressions

--- scripts/shell/setup.sh
+++ scripts/shell/setup.sh
@@ -19,15 +19,15 @@ echo ""
 echo "Welcome to the SpatialOS GDK for Unity FPS Starter Project setup script." 
 echo ""
 
-readonly RAW_DIR="$(dirname "$0")/../../../gdk-for-unity"
+readonly RAW_DIR="$(dirname "${0}")/../../../gdk-for-unity"
 readonly TARGET_DIRECTORY="$(realpath "${RAW_DIR}")"
 
-echo_with_color "This script will create the following directory: ${TARGET_DIRECTORY}" "$LOG_WARNING"
-echo_with_color "*** If such a directory already exists it will be deleted. ***" "$LOG_EMPHASIS"
-echo_with_color "Are you ready to proceed? (y/n)" "$LOG_WARNING"
+echo_with_color "This script will create the following directory: ${TARGET_DIRECTORY}" "${LOG_WARNING}"
+echo_with_color "*** If such a directory already exists it will be deleted. ***" "${LOG_EMPHASIS}"
+echo_with_color "Are you ready to proceed? (y/n)" "${LOG_WARNING}"
 
 read -r -p "" response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+if [[ "${response}" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
     echo "" > /dev/null
 else
@@ -41,40 +41,40 @@ SSH_URI="git@github.com:spatialos/gdk-for-unity.git"
 PS3="Please make a selection: "
 echo "Select a cloning method."
 echo ""
-HTTPS_OPTION="HTTPS ($HTTPS_URI)"
-SSH_OPTION="SSH   ($SSH_URI)"
-options=("$HTTPS_OPTION" "$SSH_OPTION" "Quit")
+HTTPS_OPTION="HTTPS (${HTTPS_URI})"
+SSH_OPTION="SSH   (${SSH_URI})"
+options=("${HTTPS_OPTION}" "${SSH_OPTION}" "Quit")
 select opt in "${options[@]}"
 do
-    case $opt in
-        "$HTTPS_OPTION")
-            CLONE_URI=$HTTPS_URI
+    case ${opt} in
+        "${HTTPS_OPTION}")
+            CLONE_URI=${HTTPS_URI}
             break
             ;;
-        "$SSH_OPTION")
-            CLONE_URI=$SSH_URI
+        "${SSH_OPTION}")
+            CLONE_URI=${SSH_URI}
             break
             ;;
         "Quit")
             echo "Stopping the setup process."
             exit 0
             ;;
-        *) echo "Invalid option $REPLY";;
+        *) echo "Invalid option ${REPLY}";;
     esac
 done
 
 if [[ -d "${TARGET_DIRECTORY}" ]]; then
-    echo_with_color "Deleting existing directory at ${TARGET_DIRECTORY}..." "$LOG_WARNING"
+    echo_with_color "Deleting existing directory at ${TARGET_DIRECTORY}..." "${LOG_WARNING}"
     rm -rf "${TARGET_DIRECTORY}"
 fi
 
 echo "Cloning SpatialOS GDK for Unity"
-git clone $CLONE_URI "${TARGET_DIRECTORY}"
+git clone ${CLONE_URI} "${TARGET_DIRECTORY}"
 
-PINNED_VERSION=$(cat "$(dirname "$0")/../../gdk.pinned")
+PINNED_VERSION=$(cat "$(dirname "${0}")/../../gdk.pinned")
 
 pushd "${TARGET_DIRECTORY}"
     git checkout "${PINNED_VERSION}"
 popd
 
-echo_with_color "Finished!" "$LOG_SUCCESS"
+echo_with_color "Finished!" "${LOG_SUCCESS}"
