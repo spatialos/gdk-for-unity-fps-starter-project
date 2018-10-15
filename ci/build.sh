c868bc0e71e20178e4acfdba4753e21f3ca5fb1d
Jamie Brynes <jamiebrynes@improbable.io>
2018-10-15 11:19:01 +0100

    factor out into variable

--- ci/build.sh
+++ ci/build.sh
@@ -11,6 +11,18 @@ source .shared-ci/scripts/profiling.sh
 
 UNITY_PROJECT_DIR="$(pwd)/workers/unity"
 
+# The asset cache ip cannot be hardcoded and so is stored in an environment variable on the build agent.
+# This is bash shorthand syntax for if-else predicated on the existance of the environment variable 
+# where the else branch assigns an empty string.
+#   i.e. - 
+#   if [ -z ${UNITY_ASSET_CACHE_IP} ]; then
+#       ASSET_CACHE_ARG="-CacheServerIPAddress ${UNITY_ASSET_CACHE_IP}"
+#   else
+#       ASSET_CACHE_ARG=""
+#   fi
+
+ASSET_CACHE_ARG=${UNITY_ASSET_CACHE_IP:+-CacheServerIPAddress "${UNITY_ASSET_CACHE_IP}"}
+
 markStartOfBlock "$0"
 
 markStartOfBlock "Building UnityClient"
@@ -22,7 +34,7 @@ pushd "${UNITY_PROJECT_DIR}"
         -quit \
         -logfile "$(pwd)/../../logs/UnityClientBuild.log" \
         -executeMethod "Improbable.Gdk.BuildSystem.WorkerBuilder.Build" \
-        ${UNITY_ASSET_CACHE_IP:+-CacheServerIPAddress "${UNITY_ASSET_CACHE_IP}"} \
+        ${ASSET_CACHE_ARG} \
         +buildWorkerTypes "UnityClient" \
         +buildTarget "cloud"
 popd
@@ -38,7 +50,7 @@ pushd "${UNITY_PROJECT_DIR}"
         -quit \
         -logfile "$(pwd)/../../logs/UnityGameLogicBuild.log" \
         -executeMethod "Improbable.Gdk.BuildSystem.WorkerBuilder.Build" \
-        ${UNITY_ASSET_CACHE_IP:+-CacheServerIPAddress "${UNITY_ASSET_CACHE_IP}"} \
+        ${ASSET_CACHE_ARG} \
         +buildWorkerTypes "UnityGameLogic" \
         +buildTarget "cloud"
 popd
@@ -54,7 +66,7 @@ pushd "${UNITY_PROJECT_DIR}"
         -quit \
         -logfile "$(pwd)/../../logs/SimulatedPlayerCoordinatorBuild.log" \
         -executeMethod "Improbable.Gdk.BuildSystem.WorkerBuilder.Build" \
-        ${UNITY_ASSET_CACHE_IP:+-CacheServerIPAddress "${UNITY_ASSET_CACHE_IP}"} \
+        ${ASSET_CACHE_ARG} \
         +buildWorkerTypes "SimulatedPlayerCoordinator" \
         +buildTarget "cloud"
 popd
