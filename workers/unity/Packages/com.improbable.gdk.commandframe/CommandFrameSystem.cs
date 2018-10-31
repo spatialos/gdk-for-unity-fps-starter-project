e398d9971cd0e2efed047129ad94df31e1e9a3bc
Matt Young <matthewyoung@improbable.io>
2018-10-31 11:20:09 +0000

    Create CommandFrameSystem

---
+++ workers/unity/Packages/com.improbable.gdk.commandframe/CommandFrameSystem.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class CommandFrameSystem : ComponentSystem
+{
+    public int CurrentFrame = 0;
+    public float FrameLength = 1 / 1f;
+
+    private float remainder = 0;
+
+    protected override void OnUpdate()
+    {
+        remainder += Time.deltaTime;
+
+        if (remainder > FrameLength)
+        {
+            remainder -= FrameLength;
+            CurrentFrame += 1;
+        }
+    }
+}
