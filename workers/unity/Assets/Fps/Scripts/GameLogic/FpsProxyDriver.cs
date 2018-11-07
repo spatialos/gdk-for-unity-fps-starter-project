421defdfe9adcad5b080f095ddbfec42f9ce59f6
Matt Young <matthewyoung@improbable.io>
2018-11-07 18:31:52 +0000

    Added super simple interp on client & third person characters. also stop dilation adjustment before first frame (ie before the buffer has filled up for the first time

--- workers/unity/Assets/Fps/Scripts/GameLogic/FpsProxyDriver.cs
+++ workers/unity/Assets/Fps/Scripts/GameLogic/FpsProxyDriver.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 [RequireComponent(typeof(FpsAnimator), typeof(MyProxyMovementDriver))]
 public class FpsProxyDriver : MonoBehaviour
 {
+    [SerializeField] public GameObject ControllerProxy;
+
     private FpsAnimator animator;
     private MyProxyMovementDriver movement;
     private CommandFrameSystem commandFrame;
+
+    private Vector3 velocity;
 
     private void Awake()
     {
         animator = GetComponent<FpsAnimator>();
         movement = GetComponent<MyProxyMovementDriver>();
+    }
+
+    private void Start()
+    {
+        ControllerProxy.transform.parent = null;
+        movement.Controller = ControllerProxy.GetComponent<CharacterController>();
     }
 
     private void Update()
@@ -20,5 +30,10 @@ public class FpsProxyDriver : MonoBehaviour
         animator.SetGrounded(MyMovementUtils.IsGrounded(movement.Controller));
         animator.SetMovement(movement.GetVelocity());
         animator.SetPitch(movement.GetPitch());
+
+        var newPosition = Vector3.Lerp(transform.position, ControllerProxy.transform.position, 0.5f);
+        velocity = (newPosition - transform.position) / Time.deltaTime;
+        transform.position = newPosition;
+        transform.rotation = Quaternion.Slerp(transform.rotation, ControllerProxy.transform.rotation, 0.5f);
     }
 }
