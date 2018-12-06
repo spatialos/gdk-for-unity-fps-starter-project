60cca92fa1baaf28d53dcc98ce1ac99d1950dede
jchapmanio <39483705+jchapmanio@users.noreply.github.com>
2018-12-06 16:36:11 +0000

    Begun work on controls wrapper

---
+++ workers/unity/Assets/Fps/Scripts/GameLogic/PlayerControls/IControlProvider.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IControlProvider
+{
+
+    Vector3 GetMovement();
+    Vector3 GetLook();
+
+    bool GetJumpPressed();
+    bool GetJumpPressedThisFrame();
+    bool GetJumpReleasedThisFrame();
+
+    bool GetMenuPressed();
+    bool GetMenuPressedThisFrame();
+    bool GetMenuReleasedThisFrame();
+}
+
+public class MobileControlProvider : IControlProvider
+{
+    public Vector3 GetMovement()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Vector3 GetLook()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool GetJumpPressed()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool GetJumpPressedThisFrame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool GetJumpReleasedThisFrame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool GetMenuPressed()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool GetMenuPressedThisFrame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool GetMenuReleasedThisFrame()
+    {
+        throw new System.NotImplementedException();
+    }
+}
