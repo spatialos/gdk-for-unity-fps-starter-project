edd30c08ce57d3d31fe50b086ea628b15f44613c
jchapmanio <39483705+jchapmanio@users.noreply.github.com>
2018-12-06 23:52:41 +0000

    Added mobile controller with move and look hooked up, using OnDrag to detect movement (might be unresponsive).

---
+++ workers/unity/Assets/Fps/Scripts/GameLogic/Hud/Mobile/MobileUIController.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MobileUIController : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
+    public bool AreMoving;
+    public bool AreLooking;
+    public bool AreAiming;
+    public bool JumpPressed;
+
+    public StandardButton MoveButton;
+    public StandardButton LookButton;
+    public StandardButton JumpButton;
+
+    public Vector2 moveDelta;
+    public Vector2 moveTotal;
+    private int moveTouchId;
+    private Vector2 moveStartPoint;
+
+    public Vector2 lookDelta;
+    public Vector2 lookTotal;
+    private int lookTouchId;
+    private Vector2 lookStartPoint;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        var pointOnLeftOfScreen = eventData.position.x < Screen.width / 2f;
+        if (pointOnLeftOfScreen)
+        {
+            if (!AreMoving)
+            {
+                StartTrackingMove(eventData);
+            }
+            else if (!AreLooking)
+            {
+                StartTrackingLook(eventData);
+            }
+        }
+        else
+        {
+            if (!AreLooking)
+            {
+                StartTrackingLook(eventData);
+            }
+            else if (!AreMoving)
+            {
+                StartTrackingMove(eventData);
+            }
+        }
+
+        Debug.Log(
+            $"OnPointerDown id " +
+            $"{eventData.pointerId} on " +
+            $"{(pointOnLeftOfScreen ? "Left" : "Right")} " +
+            $"of screen.");
+    }
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log($"Dragging with id {eventData.pointerId}");
+        if (AreMoving && eventData.pointerId == moveTouchId)
+        {
+            moveDelta = eventData.delta;
+            moveTotal = eventData.position - moveStartPoint;
+            Debug.Log($"moveDelta: {moveDelta} moveTotal: {moveTotal}");
+        }
+
+        if (AreLooking && eventData.pointerId == lookTouchId)
+        {
+            lookDelta = eventData.delta;
+            lookTotal = eventData.position - lookStartPoint;
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (AreMoving && eventData.pointerId == moveTouchId)
+        {
+            StopTrackingMove();
+        }
+
+        if (AreLooking && eventData.pointerId == lookTouchId)
+        {
+            StopTrackingLook();
+        }
+    }
+
+    private void StartTrackingMove(PointerEventData eventData)
+    {
+        AreMoving = true;
+        moveTouchId = eventData.pointerId;
+        moveStartPoint = eventData.position;
+        Debug.Log($"Tracking movement for id {moveTouchId}");
+    }
+
+    private void StartTrackingLook(PointerEventData eventData)
+    {
+        AreLooking = true;
+        lookTouchId = eventData.pointerId;
+        lookStartPoint = eventData.position;
+    }
+
+    private void StopTrackingMove()
+    {
+        AreMoving = false;
+        moveDelta = Vector2.zero;
+        moveTotal = Vector2.zero;
+    }
+
+    private void StopTrackingLook()
+    {
+        lookDelta = Vector2.zero;
+        lookTotal = Vector2.zero;
+        AreLooking = false;
+    }
+}
--- workers/unity/Assets/Fps/Scripts/GameLogic/PlayerControls/MobileControls.cs
+++ workers/unity/Assets/Fps/Scripts/GameLogic/PlayerControls/MobileControls.cs
@@ -1,5 +1,52 @@
 ﻿using UnityEngine;
 
-public class MobileControls : MonoBehaviour
+public class MobileControls : MonoBehaviour, IControlProvider
 {
+    private MobileUIController ui;
+
+    // Mobile controls polls state of the UI control class and filters data to be used by other classes
+    // UI control class subscribes to buttons underneath it
+    public float MovementScalar = 0.01f;
+    public float LookScalar = 0.33f;
+
+    public Vector3 Movement
+    {
+        get
+        {
+            var totalDistance = ui.moveTotal.magnitude;
+            var speed = Mathf.Min(totalDistance, 1f / MovementScalar) * MovementScalar;
+            return ui.AreMoving
+                ? new Vector3(ui.moveTotal.x, 0, ui.moveTotal.y).normalized * speed
+                : Vector3.zero;
+        }
+    }
+
+    public bool MenuPressed { get; }
+    public bool RespawnPressed { get; }
+    public float YawDelta => ui.AreLooking ? ui.lookDelta.x * LookScalar : 0;
+    public float PitchDelta => ui.AreLooking ? ui.lookDelta.y * LookScalar : 0;
+    public bool AreAiming { get; }
+    public bool AreSprinting { get; }
+    public bool JumpPressed { get; }
+    public bool ShootPressed { get; }
+    public bool ShootHeld { get; }
+    public bool ConnectPressed { get; }
+
+    private void Awake()
+    {
+        ui = FindObjectOfType<MobileUIController>();
+        if (!ui)
+        {
+            Debug.LogError("Failed to find MobileUIController");
+        }
+        else
+        {
+            Debug.Log($"ui set to {ui.gameObject.name}");
+        }
+    }
+
+    private void LateUpdate()
+    {
+        ui.lookDelta = Vector2.zero;
+    }
 }
