e011e72d1ac97ea9d0bfe57ae8077f5318c57c2e
Andrew Lee <andrew@improbable.io>
2018-12-10 18:12:29 +0000

    Upgraded AdvancedEntityPipeline to GDK V2

--- workers/unity/Assets/Fps/Scripts/SetupLogic/AdvancedEntityPipeline.cs
+++ workers/unity/Assets/Fps/Scripts/SetupLogic/AdvancedEntityPipeline.cs
@@ -1,9 +1,10 @@
-﻿using Improbable;
+﻿using System.Collections.Generic;
+using Improbable;
 using Improbable.Gdk.Core;
 using Improbable.Gdk.GameObjectCreation;
 using Improbable.Gdk.Movement;
 using Improbable.Gdk.StandardTypes;
-using Improbable.Worker;
+using Improbable.Gdk.Subscriptions;
 using UnityEngine;
 
 public class AdvancedEntityPipeline : IEntityGameObjectCreator
@@ -19,6 +20,8 @@ public class AdvancedEntityPipeline : IEntityGameObjectCreator
     private readonly string workerIdAttribute;
     private readonly Worker worker;
 
+    private readonly Dictionary<EntityId, GameObject> playerEntityObjects = new Dictionary<EntityId, GameObject>();
+
     public AdvancedEntityPipeline(Worker worker, string authPlayer, string nonAuthPlayer,
         IEntityGameObjectCreator fallback)
     {
@@ -29,41 +32,36 @@ public class AdvancedEntityPipeline : IEntityGameObjectCreator
         cachedNonAuthPlayer = Resources.Load<GameObject>(nonAuthPlayer);
     }
 
-    public GameObject OnEntityCreated(SpatialOSEntity entity)
+    public void OnEntityCreated(SpatialOSEntity entity, EntityGameObjectLinker linker)
     {
         if (!entity.HasComponent<Metadata.Component>())
         {
-            return null;
+            return;
         }
 
         var prefabName = entity.GetComponent<Metadata.Component>().EntityType;
         if (prefabName.Equals(PlayerMetadata))
         {
-            var clientMovement = entity.GetComponent<ClientMovement.Component>();
-            if (entity.GetComponent<EntityAcl.Component>().ComponentWriteAcl
-                .TryGetValue(clientMovement.ComponentId, out var clientMovementWrite))
-            {
-                var authority = false;
-                foreach (var attributeSet in clientMovementWrite.AttributeSet)
-                {
-                    if (attributeSet.Attribute.Contains(workerIdAttribute))
-                    {
-                        authority = true;
-                    }
-                }
+            var gameObject = CreatePlayerEntity(entity);
+            playerEntityObjects.Add(entity.SpatialOSEntityId, gameObject);
+            linker.LinkGameObjectToSpatialOSEntity(entity.SpatialOSEntityId, gameObject);
 
-                var serverPosition = entity.GetComponent<ServerMovement.Component>();
-                var position = serverPosition.Latest.Position.ToVector3() + worker.Origin;
-
-                var prefab = authority ? cachedAuthPlayer : cachedNonAuthPlayer;
-                var gameObject = Object.Instantiate(prefab, position, Quaternion.identity);
-
-                gameObject.name = GetGameObjectName(prefab, entity, worker);
-                return gameObject;
-            }
+            return;
         }
 
-        return fallback.OnEntityCreated(entity);
+        fallback.OnEntityCreated(entity, linker);
+    }
+
+    public void OnEntityRemoved(EntityId entityId)
+    {
+        if (playerEntityObjects.TryGetValue(entityId, out var gameObject))
+        {
+            Object.Destroy(gameObject);
+            playerEntityObjects.Remove(entityId);
+            return;
+        }
+
+        fallback.OnEntityRemoved(entityId);
     }
 
     private static string GetGameObjectName(GameObject prefab, SpatialOSEntity entity, Worker worker)
@@ -71,8 +69,46 @@ public class AdvancedEntityPipeline : IEntityGameObjectCreator
         return string.Format(GameobjectNameFormat, prefab.name, entity.SpatialOSEntityId, worker.WorkerType);
     }
 
-    public void OnEntityRemoved(EntityId entityId, GameObject linkedGameObject)
+    private GameObject CreatePlayerEntity(SpatialOSEntity entity)
     {
-        fallback.OnEntityRemoved(entityId, linkedGameObject);
+        var isAuthoritative = IsPlayerAuthoritative(entity);
+
+        var serverPosition = entity.GetComponent<ServerMovement.Component>();
+        var position = serverPosition.Latest.Position.ToVector3() + worker.Origin;
+
+        var prefab = isAuthoritative ? cachedAuthPlayer : cachedNonAuthPlayer;
+        var gameObject = Object.Instantiate(prefab, position, Quaternion.identity);
+
+        gameObject.name = GetGameObjectName(prefab, entity, worker);
+
+        return gameObject;
+    }
+
+    private bool IsPlayerAuthoritative(SpatialOSEntity entity)
+    {
+        if (!entity.HasComponent<EntityAcl.Component>())
+        {
+            Debug.LogErrorFormat("Failed to determine authority of player {0} as there was no EntityAcl at " +
+                "the time of creation.", entity.SpatialOSEntityId.Id);
+            return false;
+        }
+
+        if (!entity.GetComponent<EntityAcl.Component>().ComponentWriteAcl.TryGetValue(ClientMovement.ComponentId,
+            out var clientMovementWrite))
+        {
+            Debug.LogErrorFormat("Failed to determine authority of player {0} as the EntityAcl did not contain the" +
+                "ClientMovement component.", entity.SpatialOSEntityId.Id);
+            return false;
+        }
+
+        foreach (var attributeSet in clientMovementWrite.AttributeSet)
+        {
+            if (attributeSet.Attribute.Contains(workerIdAttribute))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
