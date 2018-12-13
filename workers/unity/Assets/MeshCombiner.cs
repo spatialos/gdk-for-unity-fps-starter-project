0c1d73e94a39b2578357af301a01c17d8c19aee6
jchapmanio <39483705+jchapmanio@users.noreply.github.com>
2018-12-13 16:59:35 +0000

    MeshCombiner now applies to  children of component

--- workers/unity/Assets/MeshCombiner.cs
+++ workers/unity/Assets/MeshCombiner.cs
@@ -1,32 +1,203 @@
-﻿using System.Collections.Generic;
-using Fps;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class MeshCombiner : MonoBehaviour
 {
-    private void Start()
+    public bool ignoreLods;
+
+    private void Awake()
     {
-        var mapBuilder = FindObjectOfType<MapBuilder>();
+        CombineMeshes(transform);
+    }
 
-        var tileEnablers = mapBuilder.GetComponentsInChildren<TileEnabler>();
+    private void CombineMeshes(Transform root)
+    {
+        // Store position
+        var rootInitPos = root.transform.position;
+        var rootInitRot = root.transform.rotation;
 
-        var tileMeshMap = new Dictionary<TileEnabler, CombineInstance[]>();
+        // Reset to identity
+        root.transform.position = Vector3.zero;
+        root.transform.rotation = Quaternion.identity;
 
-        for (var i = 0; i < tileEnablers.Length; i++)
+        // Gather + combine
+        var materialsAndMeshes = GatherMaterialsAndMeshes(root);
+        var allMaterialsCombined = CombineMaterialsAndMeshes(root, materialsAndMeshes);
+
+        // Apply to components
+        var meshFilter = root.GetComponent<MeshFilter>();
+        if (meshFilter == null)
         {
-            var meshFilters = tileEnablers[i].GetComponentsInChildren<MeshFilter>();
+            meshFilter = root.gameObject.AddComponent<MeshFilter>();
+        }
 
-            var meshes = new Mesh[meshFilters.Length];
+        meshFilter.sharedMesh = allMaterialsCombined;
 
-            var meshCombineInstances = new CombineInstance[meshFilters.Length];
+        var meshRenderer = root.GetComponent<MeshRenderer>();
+        if (meshRenderer == null)
+        {
+            meshRenderer = root.gameObject.AddComponent<MeshRenderer>();
+        }
 
-            for (var j = 0; j < meshFilters.Length; j++)
+        meshRenderer.sharedMaterials = materialsAndMeshes.Keys.ToArray();
+
+        // Restore position
+        root.transform.position = rootInitPos;
+        root.transform.rotation = rootInitRot;
+    }
+
+    private static Mesh CombineMaterialsAndMeshes(Transform root,
+        Dictionary<Material, List<CombineInstance>> materialsAndMeshes)
+    {
+        var materials = materialsAndMeshes.Keys;
+
+        var singleMaterialsCombinedInstances = new List<CombineInstance>();
+
+        foreach (var material in materials)
+        {
+            var sourceMeshes = materialsAndMeshes[material];
+            var singleMaterialCombined = new Mesh();
+            singleMaterialCombined.CombineMeshes(sourceMeshes.ToArray());
+
+            var singleMaterialCombinedInstance = new CombineInstance();
+            singleMaterialCombinedInstance.mesh = singleMaterialCombined;
+            singleMaterialCombinedInstance.transform = root.localToWorldMatrix;
+            singleMaterialsCombinedInstances.Add(singleMaterialCombinedInstance);
+        }
+
+        var allMaterialsCombined = new Mesh();
+        allMaterialsCombined.CombineMeshes(singleMaterialsCombinedInstances.ToArray(), false);
+        return allMaterialsCombined;
+    }
+
+    private Dictionary<Material, List<CombineInstance>> GatherMaterialsAndMeshes(Transform root)
+    {
+        var meshFilters = root.GetComponentsInChildren<MeshFilter>();
+        var matsToMeshes = new Dictionary<Material, List<CombineInstance>>();
+        for (var j = 0; j < meshFilters.Length; j++)
+        {
+            var meshFilter = meshFilters[j];
+            if (meshFilter.gameObject.name.Contains("LOD"))
             {
-                var cm = new CombineInstance();
-                cm.mesh = meshFilters[j].sharedMesh;
-                cm.subMeshIndex = 0;
-                meshCombineInstances[j] = cm;
+                if (ignoreLods)
+                {
+                    continue;
+                }
+
+                var highestLOD = GetHighestLOD(meshFilter.transform.parent);
+                var lowestLOD = GetLowestLOD(meshFilter.transform.parent);
+                var thisLOD = GetLODFromName(meshFilter.gameObject.name);
+
+                if (thisLOD != lowestLOD)
+                {
+                    var collider = meshFilter.GetComponent<Collider>();
+                    if (collider)
+                    {
+                        collider.enabled = false;
+                    }
+                }
+
+                if (thisLOD != highestLOD)
+                {
+                    meshFilter.GetComponent<MeshRenderer>().enabled = false;
+                    continue;
+                }
+            }
+
+
+            var meshRenderer = meshFilter.GetComponent<MeshRenderer>();
+
+            // TODO Look at grabbing more than 1 material.
+
+            var material = meshRenderer.sharedMaterial;
+            var combineInstance = new CombineInstance();
+
+
+            var objMesh = meshFilter.mesh;
+            ApplyScaleToVertexColours(meshFilter.transform.localScale, ref objMesh);
+
+            combineInstance.mesh = objMesh;
+            combineInstance.transform = meshFilter.transform.localToWorldMatrix;
+
+            if (matsToMeshes.ContainsKey(material))
+            {
+                matsToMeshes[material].Add(combineInstance);
+            }
+            else
+            {
+                matsToMeshes.Add(material, new List<CombineInstance>());
+                matsToMeshes[material].Add(combineInstance);
+            }
+
+
+            meshFilter.GetComponent<MeshRenderer>().enabled = false;
+        }
+
+        return matsToMeshes;
+    }
+
+    private int GetHighestLOD(Transform lodParent)
+    {
+        var highest = 0;
+        for (var i = 0; i < lodParent.childCount; i++)
+        {
+            var lodNum = GetLODFromName(lodParent.GetChild(i).name);
+            if (lodNum > highest)
+            {
+                highest = lodNum;
             }
         }
+
+        return highest;
+    }
+
+    private int GetLowestLOD(Transform lodParent)
+    {
+        var lowest = int.MaxValue;
+        for (var i = 0; i < lodParent.childCount; i++)
+        {
+            var lodNum = GetLODFromName(lodParent.GetChild(i).name);
+            if (lodNum < lowest)
+            {
+                lowest = lodNum;
+            }
+        }
+
+        return lowest;
+    }
+
+    public int MaxBakedScaleSize = 36;
+
+    private void ApplyScaleToVertexColours(Vector3 objLocalScale, ref Mesh mesh)
+    {
+        var colors = new Color[mesh.vertexCount];
+
+        var objCol = new Color
+        {
+            r = Mathf.Clamp(objLocalScale.x / MaxBakedScaleSize, 0f, 1f),
+            g = Mathf.Clamp(objLocalScale.y / MaxBakedScaleSize, 0f, 1f),
+            b = Mathf.Clamp(objLocalScale.z / MaxBakedScaleSize, 0f, 1f)
+        };
+
+        for (var i = 0; i < colors.Length; i++)
+        {
+            colors[i] = objCol;
+        }
+
+        mesh.colors = colors;
+    }
+
+    private int GetLODFromName(string name)
+    {
+        Debug.Assert(name.Contains("LOD"));
+        var pieces = name.Split(new string[] { "LOD" }, StringSplitOptions.RemoveEmptyEntries);
+        if (pieces.Length <= 1)
+        {
+            throw new ArgumentException($"Expected more than one part to LOD obj name {name} once split ");
+        }
+
+        return int.Parse(pieces[pieces.Length - 1]);
     }
 }
