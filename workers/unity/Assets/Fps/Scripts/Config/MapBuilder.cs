6085ec925888f7f59d67227ba2385e5a1db9895f
jchapmanio <39483705+jchapmanio@users.noreply.github.com>
2019-01-02 16:39:42 +0000

    Refactoried MapBuilder for readability. Made thick walls at edges, added corners, maps now build around parent position/rotation instead of at origin.

--- workers/unity/Assets/Fps/Scripts/Config/MapBuilder.cs
+++ workers/unity/Assets/Fps/Scripts/Config/MapBuilder.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Fps;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -12,32 +13,33 @@ public class MapBuilder : MonoBehaviour
     public float EmptyTileChance = 0.2f;
 
     private const int TileSeparation = 36;
-    private const float GroundHeight = 0f;
-    private const float HeightOffset = 0.5f;
 
     private int groundWidth;
-    private const int WallHeight = 4;
+    private const int BlockSize = 4;
 
     private GameObject[] centreTiles;
     private GameObject[] levelTiles;
     private GameObject groundTile;
     private GameObject groundEdge;
     private GameObject surroundWall;
+    private GameObject cornerPiece;
 
     private Transform tileParentTransform;
     private Transform groundParentTransform;
     private Transform surroundParentTransform;
     private Transform cubeParentTransform;
+    private Transform spawnPointSystemTransform;
 
     private const string TileParentName = "TileParent";
     private const string GroundParentName = "GroundParent";
     private const string SurroundParentName = "SurroundParent";
-    private const string CubeParentName = "CubeParent";
+    private const string SpawnPointSystemName = "SpawnPointSystem";
 
     private const string LevelTilePath = "Prefabs/Level/Tiles";
     private const string GroundTilePath = "Prefabs/Level/Ground/Ground4x4";
     private const string GroundEdgePath = "Prefabs/Level/Ground/Edge";
     private const string SurroundPath = "Prefabs/Level/Surround/Wall";
+    private const string CornerPath = "Prefabs/Level/Surround/Corner";
 
     // Central tiles are hardcoded.
     private const string CentreTile0 = "Prefabs/Level/Tiles/Centre0";
@@ -45,20 +47,82 @@ public class MapBuilder : MonoBehaviour
     private const string CentreTile2 = "Prefabs/Level/Tiles/Centre2";
     private const string CentreTile3 = "Prefabs/Level/Tiles/Centre3";
 
+    private int groundLayers => (Layers - 1) / 4 + 1;
+
 #if UNITY_EDITOR
-    public void Build()
+    public void CleanAndBuild()
     {
+        if (TryLoadResources() == false)
+        {
+            Debug.LogError("Generation aborted due to error");
+            return;
+        }
+
+        Clean();
+
+        InitializeGroupsAndComponents();
         Random.InitState(Seed.GetHashCode());
+
+        var originalPosition = transform.position;
+        var originalRotation = transform.rotation;
+        transform.position = Vector3.zero;
+        transform.rotation = Quaternion.identity;
         PlaceTiles();
         PlaceGround();
-        PlaceSurround();
+        FillSurround();
         MakeLevelObjectStatic();
-        Debug.Log($"Finished building world of size: {groundWidth} x {groundWidth}");
+
+        transform.position = originalPosition;
+        transform.rotation = originalRotation;
+
+        var worldSize = groundLayers * TileSeparation * 4 * 2 + 4;
+        Debug.Log($"Finished building world of size: {worldSize} x {worldSize}");
     }
 
-    private void PlaceTiles()
+    private void InitializeGroupsAndComponents()
     {
-        centreTiles = new GameObject[]
+        if (!gameObject.GetComponent<TileSettings>())
+        {
+            gameObject.AddComponent<TileSettings>();
+        }
+
+        if (!GetComponentInChildren<SpawnPoints>())
+        {
+            spawnPointSystemTransform = MakeGroup(SpawnPointSystemName);
+            spawnPointSystemTransform.gameObject.AddComponent<SpawnPoints>();
+        }
+
+        groundParentTransform = MakeGroup(GroundParentName);
+
+        surroundParentTransform = MakeGroup(SurroundParentName);
+
+        tileParentTransform = MakeGroup(TileParentName);
+        tileParentTransform.gameObject.AddComponent<TileCollapser>();
+    }
+
+    private bool TryLoadResources()
+    {
+        if (!TryLoadResource(GroundTilePath, out groundTile))
+        {
+            return false;
+        }
+
+        if (!TryLoadResource(GroundEdgePath, out groundEdge))
+        {
+            return false;
+        }
+
+        if (!TryLoadResource(SurroundPath, out surroundWall))
+        {
+            return false;
+        }
+
+        if (!TryLoadResource(CornerPath, out cornerPiece))
+        {
+            return false;
+        }
+
+        centreTiles = new[]
         {
             Resources.Load<GameObject>(CentreTile0),
             Resources.Load<GameObject>(CentreTile1),
@@ -68,21 +132,115 @@ public class MapBuilder : MonoBehaviour
 
         if (centreTiles.Any(t => t == null))
         {
-            Debug.LogError("Central tile prefabs not loaded.");
-            return;
+            Debug.LogError("Failed to load CentreTile# resources");
+            return false;
         }
 
         levelTiles = Resources.LoadAll<GameObject>(LevelTilePath);
 
         if (levelTiles.Length <= 0)
         {
-            Debug.LogError("Tile prefabs not loaded.");
-            return;
+            Debug.LogError($"Failed to load resource at {LevelTilePath}");
+            return false;
         }
 
-        tileParentTransform = new GameObject(TileParentName).transform;
-        tileParentTransform.parent = gameObject.transform;
+        return true;
+    }
 
+    private bool TryLoadResource(string resourcePath, out GameObject resource)
+    {
+        resource = Resources.Load<GameObject>(resourcePath);
+        if (resource != null)
+        {
+            return true;
+        }
+
+        Debug.LogError($"Failed to load resource at {resourcePath}");
+        return false;
+    }
+
+    private Transform MakeGroup(string groupName)
+    {
+        var group = new GameObject(groupName).transform;
+        group.parent = transform;
+        group.localPosition = Vector3.zero;
+        group.localRotation = Quaternion.identity;
+        group.localScale = Vector3.one;
+        return group;
+    }
+
+    private void FillSurround()
+    {
+        for (var i = -groundLayers; i < groundLayers; i++)
+        {
+            var offset = TileSeparation * 4 * i + TileSeparation * 2;
+            MakeEdge(offset, 0);
+            MakeEdge(offset, 90);
+            MakeEdge(offset, 180);
+            MakeEdge(offset, 270);
+        }
+
+        for (var i = 0; i < 360; i += 90)
+        {
+            MakeCorner(i);
+        }
+    }
+
+    private void MakeEdge(float offset, int angle)
+    {
+        var rotation = Quaternion.Euler(0, angle, 0);
+
+        var floor = Instantiate(groundEdge,
+            rotation * new Vector3(offset, 0, groundLayers * TileSeparation * 4 + BlockSize * 0.25f),
+            rotation * Quaternion.Euler(90, 0, 0),
+            groundParentTransform);
+        floor.transform.localScale = new Vector3(144, 2, 1);
+
+        var wall = Instantiate(surroundWall,
+            rotation * new Vector3(offset, BlockSize * .5f, groundLayers * TileSeparation * 4 + BlockSize * .5f),
+            rotation,
+            surroundParentTransform);
+        wall.transform.localScale = new Vector3(144, 4, 1);
+
+        var wallFloor = Instantiate(groundEdge,
+            rotation * new Vector3(offset, BlockSize, groundLayers * TileSeparation * 4 + BlockSize),
+            rotation * Quaternion.Euler(90, 0, 0),
+            surroundParentTransform);
+        wallFloor.transform.localScale = new Vector3(144, 4, 1);
+
+        // Collision
+        var collisionHeight = 16;
+        var collision = Instantiate(surroundWall,
+            rotation * new Vector3(offset, BlockSize + collisionHeight * .5f,
+                groundLayers * TileSeparation * 4 + BlockSize * .5f),
+            rotation,
+            surroundParentTransform);
+        collision.transform.localScale = new Vector3(144 + 4, collisionHeight, 1); // Collisions overlap to fill corners
+        collision.gameObject.name = "Collision";
+
+        if (Application.isPlaying)
+        {
+            Destroy(collision.GetComponent<MeshRenderer>());
+            Destroy(collision.GetComponent<MeshFilter>());
+        }
+        else
+        {
+            DestroyImmediate(collision.GetComponent<MeshRenderer>());
+            DestroyImmediate(collision.GetComponent<MeshFilter>());
+        }
+    }
+
+    private void MakeCorner(int angle)
+    {
+        var rotation = Quaternion.Euler(0, angle, 0);
+        Instantiate(cornerPiece,
+            rotation * new Vector3(-groundLayers * TileSeparation * 4, 0, groundLayers * TileSeparation * 4),
+            rotation,
+            surroundParentTransform);
+    }
+
+    private void PlaceTiles()
+    {
         var tileCoord = new Vector2Int();
         var diff = new Vector2Int(0, -1);
 
@@ -91,7 +249,8 @@ public class MapBuilder : MonoBehaviour
         for (var i = 0; i < tileCount; i++)
         {
             // -layers < x <= layers AND -layers < y <= layers
-            if (-Layers < tileCoord.x && tileCoord.x <= Layers && -Layers < tileCoord.y && tileCoord.y <= Layers)
+            if (-Layers < tileCoord.x && tileCoord.x <= Layers
+                && -Layers < tileCoord.y && tileCoord.y <= Layers)
             {
                 if (i < 4)
                 {
@@ -113,10 +272,9 @@ public class MapBuilder : MonoBehaviour
             tileCoord += diff;
         }
 
-        tileParentTransform.position = new Vector3()
+        tileParentTransform.position = new Vector3
         {
             x = tileParentTransform.position.x,
-            y = HeightOffset,
             z = tileParentTransform.position.z
         };
     }
@@ -140,14 +298,14 @@ public class MapBuilder : MonoBehaviour
 
         Instantiate(
             tile,
-            new Vector3()
+            new Vector3
             {
                 x = (tileCoord.x - 1) * TileSeparation + tileOffset,
                 z = (tileCoord.y - 1) * TileSeparation + tileOffset
             },
-            new Quaternion()
+            new Quaternion
             {
-                eulerAngles = new Vector3()
+                eulerAngles = new Vector3
                 {
                     y = rotation
                 }
@@ -155,27 +313,9 @@ public class MapBuilder : MonoBehaviour
             tileParentTransform.transform);
     }
 
+
     private void PlaceGround()
     {
-        groundTile = Resources.Load<GameObject>(GroundTilePath);
-        if (groundTile == null)
-        {
-            Debug.LogError("Ground prefab not loaded.");
-            return;
-        }
-
-        groundEdge = Resources.Load<GameObject>(GroundEdgePath);
-        if (groundEdge == null)
-        {
-            Debug.LogError("Ground edge prefab not loaded.");
-            return;
-        }
-
-        groundParentTransform = new GameObject(GroundParentName).transform;
-        groundParentTransform.parent = gameObject.transform;
-
-        var groundLayers = (Layers - 1) / 4 + 1;
-
         for (var x = -groundLayers; x < groundLayers; x++)
         {
             for (var z = -groundLayers; z < groundLayers; z++)
@@ -183,150 +323,21 @@ public class MapBuilder : MonoBehaviour
                 PlaceGroundTile(x, z);
             }
         }
-
-        PlaceEdges(groundLayers);
-
-        groundParentTransform.position = new Vector3()
-        {
-            x = groundParentTransform.position.x,
-            y = HeightOffset,
-            z = groundParentTransform.position.z
-        };
     }
 
     private void PlaceGroundTile(int groundX, int groundZ)
     {
         Instantiate(
             groundTile,
-            new Vector3()
+            new Vector3
             {
-                x = 144 * groundX + 74,
-                y = GroundHeight,
-                z = 144 * groundZ + 70
+                x = groundX * 144 + 72,
+                z = groundZ * 144 + 72
             },
             Quaternion.identity,
             groundParentTransform.transform);
     }
 
-    private void PlaceEdges(int groundLayers)
-    {
-        var edgeSize = 0.4f;
-        groundWidth = groundLayers * 288 + 4;
-        var edgeScale = new Vector3()
-        {
-            x = groundWidth / 10f,
-            y = edgeSize,
-            z = edgeSize
-        };
-
-        //top edge
-        var topEdgeObject = Instantiate(
-            groundEdge,
-            new Vector3()
-            {
-                y = GroundHeight,
-                z = groundLayers * 144
-            },
-            Quaternion.identity,
-            groundParentTransform.transform);
-        topEdgeObject.transform.localScale = edgeScale;
-
-        //left edge
-        var leftEdgeObject = Instantiate(
-            groundEdge,
-            new Vector3()
-            {
-                x = -groundLayers * 144,
-                y = GroundHeight
-            },
-            new Quaternion()
-            {
-                eulerAngles = new Vector3()
-                {
-                    y = -90f
-                }
-            },
-            groundParentTransform.transform);
-        leftEdgeObject.transform.localScale = edgeScale;
-    }
-
-    private void PlaceSurround()
-    {
-        surroundParentTransform = new GameObject(SurroundParentName).transform;
-        surroundParentTransform.parent = gameObject.transform;
-
-        surroundWall = Resources.Load<GameObject>(SurroundPath);
-        if (surroundWall == null)
-        {
-            Debug.LogError("Surround wall prefab not loaded.");
-            return;
-        }
-
-        var halfSurroundWidth = groundWidth / 2;
-
-        //top
-        PlaceWall(0, halfSurroundWidth, 180f);
-
-        //bottom
-        PlaceWall(0, -halfSurroundWidth, 0f);
-
-        //left
-        PlaceWall(-halfSurroundWidth, 0, 90f);
-
-        //right
-        PlaceWall(halfSurroundWidth, 0, -90f);
-    }
-
-    private void PlaceWall(int wallX, int wallZ, float rotation)
-    {
-        var wallScale = new Vector3()
-        {
-            x = WallHeight,
-            y = groundWidth,
-            z = 1
-        };
-
-        var surroundWallObjectVisible = Instantiate(
-            surroundWall,
-            new Vector3()
-            {
-                x = wallX,
-                y = WallHeight / 2,
-                z = wallZ
-            },
-            new Quaternion()
-            {
-                eulerAngles = new Vector3()
-                {
-                    x = 180,
-                    y = rotation,
-                    z = -90
-                }
-            },
-            surroundParentTransform.transform);
-        surroundWallObjectVisible.transform.localScale = wallScale;
-
-        var surroundWallObjectInvisible = Instantiate(
-            surroundWall,
-            new Vector3()
-            {
-                x = wallX,
-                y = WallHeight * 3 / 2,
-                z = wallZ
-            },
-            new Quaternion()
-            {
-                eulerAngles = new Vector3()
-                {
-                    x = 180,
-                    y = rotation,
-                    z = -90
-                }
-            },
-            surroundParentTransform.transform);
-        surroundWallObjectInvisible.transform.localScale = wallScale;
-        surroundWallObjectInvisible.GetComponent<Renderer>().enabled = false;
-    }
 
     private void MakeLevelObjectStatic()
     {
@@ -356,12 +367,6 @@ public class MapBuilder : MonoBehaviour
             }
 
             if (child.gameObject.name.Contains(SurroundParentName))
-            {
-                childrenToDestroy.Enqueue(child.gameObject);
-                continue;
-            }
-
-            if (child.gameObject.name.Contains(CubeParentName))
             {
                 childrenToDestroy.Enqueue(child.gameObject);
                 continue;
