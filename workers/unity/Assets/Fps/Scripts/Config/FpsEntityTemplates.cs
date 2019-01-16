e2ba4cbc53d7fd406b301eefa108d12e7da391c1
Jessica Falk <jessica@improbable.io>
2019-01-16 17:47:52 +0000

    updating entity templates

--- workers/unity/Assets/Fps/Scripts/Config/FpsEntityTemplates.cs
+++ workers/unity/Assets/Fps/Scripts/Config/FpsEntityTemplates.cs
@@ -7,40 +7,47 @@ using Improbable.Gdk.Movement;
 using Improbable.Gdk.PlayerLifecycle;
 using Improbable.Gdk.StandardTypes;
 using Improbable.PlayerLifecycle;
+using UnityEngine;
 
 namespace Fps
 {
     public static class FpsEntityTemplates
     {
-        private static readonly List<string> AllWorkerAttributes =
-            new List<string> { WorkerUtils.UnityGameLogic, WorkerUtils.UnityClient, WorkerUtils.SimulatedPlayer, WorkerUtils.AndroidClient, WorkerUtils.iOSClient };
-
         public static EntityTemplate Spawner()
         {
-            const string gameLogic = WorkerUtils.UnityGameLogic;
+            var position = new Position.Snapshot { Coords = new Vector3().ToSpatialCoordinates() };
+            var metadata = new Metadata.Snapshot { EntityType = "PlayerCreator" };
 
-            return EntityBuilder.Begin()
-                .AddPosition(0, 0, 0, gameLogic)
-                .AddMetadata("PlayerCreator", gameLogic)
-                .SetPersistence(true)
-                .SetReadAcl(gameLogic)
-                .AddComponent(PlayerCreator.Component.CreateSchemaComponentData(), gameLogic)
-                .Build();
+            var template = new EntityTemplate();
+            template.AddComponent(position, WorkerUtils.UnityGameLogic);
+            template.AddComponent(metadata, WorkerUtils.UnityGameLogic);
+            template.AddComponent(new Persistence.Snapshot(), WorkerUtils.UnityGameLogic);
+            template.AddComponent(new PlayerCreator.Snapshot(), WorkerUtils.UnityGameLogic);
+
+            template.SetReadAccess(WorkerUtils.UnityGameLogic);
+            template.SetComponentWriteAccess(EntityAcl.ComponentId, WorkerUtils.UnityGameLogic);
+
+            return template;
         }
 
         public static EntityTemplate SimulatedPlayerCoordinatorTrigger()
         {
-            return EntityBuilder.Begin()
-                .AddPosition(0, 0, 0, WorkerUtils.SimulatedPlayerCoorindator)
-                .AddMetadata("SimulatedPlayerCoordinatorTrigger", WorkerUtils.SimulatedPlayerCoorindator)
-                .SetPersistence(true)
-                .SetReadAcl(WorkerUtils.SimulatedPlayerCoorindator)
-                .Build();
+            var position = new Position.Snapshot { Coords = new Vector3().ToSpatialCoordinates() };
+            var metadata = new Metadata.Snapshot { EntityType = "SimulatedPlayerCoordinatorTrigger" };
+
+            var template = new EntityTemplate();
+            template.AddComponent(position, WorkerUtils.SimulatedPlayerCoorindator);
+            template.AddComponent(metadata, WorkerUtils.SimulatedPlayerCoorindator);
+            template.AddComponent(new Persistence.Snapshot(), WorkerUtils.SimulatedPlayerCoorindator);
+
+            template.SetReadAccess(WorkerUtils.SimulatedPlayerCoorindator);
+            template.SetComponentWriteAccess(EntityAcl.ComponentId, WorkerUtils.SimulatedPlayerCoorindator);
+
+            return template;
         }
 
         public static EntityTemplate Player(string workerId, Vector3f position)
         {
-            const string gameLogic = WorkerUtils.UnityGameLogic;
             var client = $"workerId:{workerId}";
 
             var (spawnPosition, spawnYaw, spawnPitch) = SpawnPoints.GetRandomSpawnPoint();
@@ -50,43 +57,52 @@ namespace Fps
                 Position = spawnPosition.ToIntAbsolute()
             };
 
-            var serverMovement = ServerMovement.Component.CreateSchemaComponentData(serverResponse);
-            var clientMovementData = new ClientRequest();
             var rotationUpdate = new RotationUpdate
             {
                 Yaw = spawnYaw.ToInt1k(),
                 Pitch = spawnPitch.ToInt1k()
             };
-            var clientMovement = ClientMovement.Component.CreateSchemaComponentData(clientMovementData);
-            var clientRotation = ClientRotation.Component.CreateSchemaComponentData(rotationUpdate);
-            var shootingComponent = ShootingComponent.Component.CreateSchemaComponentData();
-            var gunStateComponent = GunStateComponent.Component.CreateSchemaComponentData(false);
-            var gunComponent = GunComponent.Component.CreateSchemaComponentData(PlayerGunSettings.DefaultGunIndex);
-            var maxHealth = PlayerHealthSettings.MaxHealth;
 
-            var healthComponent = HealthComponent.Component.CreateSchemaComponentData(maxHealth, maxHealth);
-            var healthRegenComponent = HealthRegenComponent.Component.CreateSchemaComponentData(false,
-                0,
-                PlayerHealthSettings.SpatialCooldownSyncInterval,
-                PlayerHealthSettings.RegenAfterDamageCooldown,
-                PlayerHealthSettings.RegenInterval,
-                PlayerHealthSettings.RegenAmount);
+            var pos = new Position.Snapshot { Coords = spawnPosition.ToSpatialCoordinates() };
+            var serverMovement = new ServerMovement.Snapshot { Latest = serverResponse };
+            var clientMovement = new ClientMovement.Snapshot { Latest = new ClientRequest() };
+            var clientRotation = new ClientRotation.Snapshot { Latest = rotationUpdate };
+            var shootingComponent = new ShootingComponent.Snapshot();
+            var gunComponent = new GunComponent.Snapshot { GunId = PlayerGunSettings.DefaultGunIndex };
+            var gunStateComponent = new GunStateComponent.Snapshot { IsAiming = false };
+            var healthComponent = new HealthComponent.Snapshot
+            {
+                Health = PlayerHealthSettings.MaxHealth,
+                MaxHealth = PlayerHealthSettings.MaxHealth,
+            };
 
-            return EntityBuilder.Begin()
-                .AddPosition(spawnPosition.x, spawnPosition.y, spawnPosition.z, gameLogic)
-                .AddMetadata("Player", gameLogic)
-                .SetPersistence(false)
-                .SetReadAcl(AllWorkerAttributes)
-                .AddComponent(serverMovement, gameLogic)
-                .AddComponent(clientMovement, client)
-                .AddComponent(clientRotation, client)
-                .AddComponent(shootingComponent, client)
-                .AddComponent(gunComponent, gameLogic)
-                .AddComponent(gunStateComponent, client)
-                .AddComponent(healthComponent, gameLogic)
-                .AddComponent(healthRegenComponent, gameLogic)
-                .AddPlayerLifecycleComponents(workerId, client, gameLogic)
-                .Build();
+            var healthRegenComponent = new HealthRegenComponent.Snapshot
+            {
+                CooldownSyncInterval = PlayerHealthSettings.SpatialCooldownSyncInterval,
+                DamagedRecently = false,
+                RegenAmount = PlayerHealthSettings.RegenAmount,
+                RegenCooldownTimer = PlayerHealthSettings.RegenAfterDamageCooldown,
+                RegenInterval = PlayerHealthSettings.RegenInterval,
+                RegenPauseTime = 0,
+            };
+
+            var template = new EntityTemplate();
+            template.AddComponent(pos, WorkerUtils.UnityGameLogic);
+            template.AddComponent(new Metadata.Snapshot { EntityType = "Player" }, WorkerUtils.UnityGameLogic);
+            template.AddComponent(serverMovement, WorkerUtils.UnityGameLogic);
+            template.AddComponent(clientMovement, client);
+            template.AddComponent(clientRotation, client);
+            template.AddComponent(shootingComponent, client);
+            template.AddComponent(gunComponent, WorkerUtils.UnityGameLogic);
+            template.AddComponent(gunStateComponent, client);
+            template.AddComponent(healthComponent, WorkerUtils.UnityGameLogic);
+            template.AddComponent(healthRegenComponent, WorkerUtils.UnityGameLogic);
+            PlayerLifecycleHelper.AddPlayerLifecycleComponents(template, workerId, client, WorkerUtils.UnityGameLogic);
+
+            template.SetReadAccess(WorkerUtils.UnityClient, WorkerUtils.UnityGameLogic, WorkerUtils.SimulatedPlayer, WorkerUtils.AndroidClient);
+            template.SetComponentWriteAccess(EntityAcl.ComponentId, WorkerUtils.UnityGameLogic);
+
+            return template;
         }
     }
 }
