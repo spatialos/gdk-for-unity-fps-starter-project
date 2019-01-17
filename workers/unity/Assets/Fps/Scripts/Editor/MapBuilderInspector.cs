9e884c05a28d684bc60555a7fb9861f5d25530fb
jchapmanio <39483705+jchapmanio@users.noreply.github.com>
2019-01-17 14:14:53 +0000

    Limit Layers to > 0. Display # tiles to be generated, highlight yellow if in danger zone. Display confirmation on generation if in danger zone.

--- workers/unity/Assets/Fps/Scripts/Editor/MapBuilderInspector.cs
+++ workers/unity/Assets/Fps/Scripts/Editor/MapBuilderInspector.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 [CustomEditor(typeof(MapBuilder))]
 public class MapBuilderInspector : Editor
 {
+    private const int WarnLayersThreshold = 25;
+
     public override void OnInspectorGUI()
     {
         var myTarget = (MapBuilder) target;
 
-        myTarget.Layers = EditorGUILayout.IntField(new GUIContent(
+        myTarget.Layers = Mathf.Max(0,
+            EditorGUILayout.IntField(new GUIContent(
                 "Number of Tile Layers",
-                "N layers corresponds to 4*(N^2) tiles."),
-            myTarget.Layers);
+                "N layers corresponds to 4*(N^2) tiles."), myTarget.Layers));
+
+        var numTiles = GetTotalTilesFromLayers(myTarget.Layers);
+
+        GUI.color = myTarget.Layers < WarnLayersThreshold ? Color.white : Color.yellow;
+        GUILayout.Label($"Number of tile to generate: {numTiles}");
+        GUI.color = Color.white;
 
         myTarget.Seed = EditorGUILayout.TextField(new GUIContent(
                 "Seed for Map Generator",
@@ -25,12 +33,26 @@ public class MapBuilderInspector : Editor
 
         if (GUILayout.Button("Generate Map"))
         {
-            myTarget.CleanAndBuild();
+            if (myTarget.Layers < WarnLayersThreshold
+                || EditorUtility.DisplayDialog("Generate Map Confirmation",
+                    $"You are generating {numTiles} tiles. This can potentially take a VERY long time, " +
+                    "and it is recommended that you save first!\n" +
+                    "Do you wish to continue?",
+                    "Continue",
+                    "Cancel"))
+            {
+                myTarget.CleanAndBuild();
+            }
         }
 
-        if (GUILayout.Button("Clean Map"))
+        if (GUILayout.Button("Clear Map"))
         {
             myTarget.Clean();
         }
+    }
+
+    private int GetTotalTilesFromLayers(int layers)
+    {
+        return Mathf.RoundToInt(Mathf.Pow(layers * 2, 2));
     }
 }
