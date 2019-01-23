e76183896c74dc4c311c1be709da58a28871140f
jchapmanio <39483705+jchapmanio@users.noreply.github.com>
2019-01-23 11:45:44 +0000

    Feature/mobile drawdistance update (#84)
    * Fix for GUIStyle not being available when scripts rebuild and throwing an exception when used
    
    * Invert `if`
    
    * Refactoring for readability, pushed all Editor-only behaviours into own functions. Fixed issues with TileSettings inspector not updating properly.
    
    * Fixed draw distance not being set correctly, refactored to have clearer naming. Removed 'Force Is Client' as not used and complicates the purpose.
    
    * Removed redundant GUI.enabled line
    
    * Removed redundant Instance ref assignment, removed #if UNITY_EDITOR directive and replaced with if (Application.isEditor) + renamed function
    
    * Renamed Preview->ShowPreview, changed inspector object casting to explicit
    
    * Feature/singleton mapqualitysettings (#88)
    
    * A number of MapQualitySettings improvements. Now a published singleton, cleans up after itself better.
    
    MapQualitySettings
    - Now a scriptable object published by MapQualitySettingsPublisher
    - Now reliably sets the global clip distance shader property back to -1 whenever a) the user returns from play mode, and b) the object is no longer being previewed
    - If game launched without a mapqualitysettings published, it will create a default instance and warn the user.
    
    Inspector
    - No longer uses GUIStyle
    - Now only shows preview mode when mapqualitysettings object is selected
    - restores project quality setting to what it was when preview was begun.
    - (while playing) Warns user if they are modifying a settings object and it is not in use by the game, and so any changes won't be visible. Also disables preview option in that situation.
    
    TileEnabler
    - Does a check on Start to see if its checkout distance has not been set by MapQualitySettings (checkoutdistancesquared == 0). If not, it requests the checkoutdistance from MapQualitySettings.  (This is done as the order of events is different depending on if a publisher is used or if a default mapqualitysettings instance is made)
    
    * Removed MapQualitySettings component from level prefabs.
    
    * Fixed changed settings not being detected by Unity save system
    
    * Added map quality settings object asset.
    
    * Added more info to warning box on mapqualitysettingsinspector (location of mapqualitysettings asset)
    
    * Cache dim colour
    
    * Turned CheckoutDistanceInternal into function GetCheckoutDistance()
    
    * Made conditional slightly nicer to interpret
    
    * Actioning review cleanups
    
    * Synced with origin and removed MapQualitySettings components from level prefabs.

---
+++ workers/unity/Assets/Fps/Scripts/Editor/GameLogic/WorldTiles/MapQualitySettingsInspector.cs
@@ -0,0 +1,213 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Fps
+{
+    [CustomEditor(typeof(MapQualitySettings))]
+    public class MapQualitySettingsInspector : Editor
+    {
+        private int activeQualityLevel;
+        private MapQualitySettings settings;
+        private int storedQualityLevel; // Used to restore project quality level when no longer previewing quality
+        private readonly Color dimColor = new Color(.77f, .77f, .77f);
+
+        private bool settingsAreCurrentlyInUse =>
+            Application.isPlaying && MapQualitySettings.Instance == settings
+            || MapQualitySettings.ShowPreview;
+
+        private void OnEnable()
+        {
+            storedQualityLevel = QualitySettings.GetQualityLevel();
+        }
+
+        private void OnDisable()
+        {
+            if (!MapQualitySettings.ShowPreview)
+            {
+                return;
+            }
+
+            MapQualitySettings.ShowPreview = false;
+            QualitySettings.SetQualityLevel(storedQualityLevel);
+            settings.Apply();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            settings = (MapQualitySettings) target;
+
+            activeQualityLevel = QualitySettings.GetQualityLevel();
+
+            DrawNotEditingActiveQualitySettingWarning();
+
+            EditorGUI.BeginChangeCheck();
+
+            RefreshQualityProperties();
+            DrawPropertiesExcluding(serializedObject,
+                serializedObject.FindProperty("Settings").name);
+
+            GUILayout.Space(10);
+
+            GUILayout.Label("Tile draw distances", EditorStyles.boldLabel);
+            DrawTileQualityProperties();
+
+            GUILayout.Space(10);
+
+            if ((!Application.isPlaying || MapQualitySettings.Instance == settings) && DrawPreviewToggle())
+            {
+                DrawQualityPreviewButtons();
+            }
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                EditorUtility.SetDirty(target);
+                settings.Apply();
+            }
+        }
+
+        private void DrawNotEditingActiveQualitySettingWarning()
+        {
+            if (!Application.isPlaying || MapQualitySettings.Instance == settings)
+            {
+                return;
+            }
+
+            var assetLocation = AssetDatabase.GetAssetPath(MapQualitySettings.Instance.GetInstanceID());
+            var locationString = string.IsNullOrEmpty(assetLocation)
+                ? "a script-instantiated MapQualitySettings object"
+                : $"the MapQualitySettings object at:-\n\t'{assetLocation}'";
+
+
+            EditorGUILayout.HelpBox("Modifying these values will not affect the running game " +
+                $"as it is using {locationString}", MessageType.Warning);
+        }
+
+        private void RefreshQualityProperties()
+        {
+            if (QualitiesPropertyMatchesProject())
+            {
+                return;
+            }
+
+            var newQualities = new List<MapQualityLevelData>();
+            foreach (var qualityName in QualitySettings.names)
+            {
+                var newTileQualitySetting = new MapQualityLevelData();
+                newTileQualitySetting.QualityName = qualityName;
+                newTileQualitySetting.CheckoutDistance = MapQualitySettings.DefaultCheckoutDistance;
+                foreach (var setting in settings.Settings)
+                {
+                    if (setting.QualityName != qualityName)
+                    {
+                        continue;
+                    }
+
+                    newTileQualitySetting.CheckoutDistance = setting.CheckoutDistance;
+                    break;
+                }
+
+                newQualities.Add(newTileQualitySetting);
+            }
+
+            settings.Settings = newQualities;
+        }
+
+        private bool QualitiesPropertyMatchesProject()
+        {
+            if (settings.Settings.Count != QualitySettings.names.Length)
+            {
+                return false;
+            }
+
+            for (var i = 0; i < QualitySettings.names.Length; i++)
+            {
+                var name1 = settings.Settings[i].QualityName;
+                var name2 = QualitySettings.names[i];
+                if (name1 != name2)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void DrawTileQualityProperties()
+        {
+            for (var i = 0; i < settings.Settings.Count; i++)
+            {
+                var qualityName = settings.Settings[i].QualityName;
+                var checkoutDistance = settings.Settings[i].CheckoutDistance;
+
+                GUI.color = settingsAreCurrentlyInUse && activeQualityLevel != i
+                    ? dimColor
+                    : Color.white;
+                settings.Settings[i].CheckoutDistance = Mathf.Max(0,
+                    EditorGUILayout.FloatField(qualityName, checkoutDistance));
+                GUI.color = Color.white;
+            }
+        }
+
+        private bool DrawPreviewToggle()
+        {
+            if (Application.isPlaying)
+            {
+                GUI.enabled = false;
+                GUILayout.Toggle(true, "Show Preview");
+                GUI.enabled = true;
+                return true;
+            }
+
+            var togglePreview = GUILayout.Toggle(MapQualitySettings.ShowPreview, "Show Preview");
+
+            if (togglePreview == MapQualitySettings.ShowPreview)
+            {
+                return togglePreview;
+            }
+
+            MapQualitySettings.ShowPreview = togglePreview;
+
+            if (togglePreview)
+            {
+                storedQualityLevel = QualitySettings.GetQualityLevel();
+            }
+            else
+            {
+                QualitySettings.SetQualityLevel(storedQualityLevel);
+            }
+
+            return togglePreview;
+        }
+
+        private void DrawQualityPreviewButtons()
+        {
+            const int verticalLayoutThreshold = 5;
+
+            GUILayout.Label("Current project quality level", EditorStyles.boldLabel);
+
+            if (QualitySettings.names.Length < verticalLayoutThreshold)
+            {
+                EditorGUILayout.BeginHorizontal();
+            }
+
+            for (var i = 0; i < QualitySettings.names.Length; i++)
+            {
+                if (!GUILayout.Toggle(i == activeQualityLevel, QualitySettings.names[i], "toolbarButton"))
+                {
+                    continue;
+                }
+
+                activeQualityLevel = i;
+                QualitySettings.SetQualityLevel(i);
+            }
+
+            if (QualitySettings.names.Length < verticalLayoutThreshold)
+            {
+                EditorGUILayout.EndHorizontal();
+            }
+
+            GUI.backgroundColor = Color.white;
+        }
+    }
+}
---
+++ workers/unity/Assets/Fps/Scripts/GameLogic/WorldTiles/MapQualitySettings.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fps
+{
+    [CreateAssetMenu(fileName = "MapQualitySettings", menuName = "Improbable/Map Quality Settings")]
+    public class MapQualitySettings : ScriptableObject
+    {
+        public const float DefaultCheckoutDistance = 60f;
+
+        public static bool ShowPreview;
+        public List<MapQualityLevelData> Settings = new List<MapQualityLevelData>();
+
+        private static MapQualitySettings instance;
+        private float checkoutDistanceCache = -1;
+
+        public static float CheckoutDistance => Instance.GetCheckoutDistance();
+
+        public static MapQualitySettings Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    CreateDefaultInstance();
+                }
+
+                return instance;
+            }
+
+            set
+            {
+                if (value == null)
+                {
+                    CreateDefaultInstance();
+                }
+                else
+                {
+                    instance = value;
+                    instance.Apply();
+                }
+            }
+        }
+
+        private static void CreateDefaultInstance()
+        {
+            Debug.LogWarning("MapQualitySettings: No settings specified. To fix this, use a " +
+                "MapQualitySettingsPublisher and ensure it has a MapQualitySettings reference.\n");
+
+            instance = CreateInstance<MapQualitySettings>();
+            instance.Apply();
+        }
+
+        private float GetCheckoutDistance()
+        {
+            if (!Application.isPlaying && !ShowPreview)
+            {
+                return -1;
+            }
+
+            if (checkoutDistanceCache > 0)
+            {
+                return checkoutDistanceCache;
+            }
+
+            var activeQualityLevelName = QualitySettings.names[QualitySettings.GetQualityLevel()];
+            foreach (var setting in Settings)
+            {
+                if (setting.QualityName != activeQualityLevelName)
+                {
+                    continue;
+                }
+
+                checkoutDistanceCache = setting.CheckoutDistance;
+                return checkoutDistanceCache;
+            }
+
+            Debug.LogWarning($"Using default draw distance of {DefaultCheckoutDistance} for ALL quality levels.\n");
+            checkoutDistanceCache = DefaultCheckoutDistance;
+            return checkoutDistanceCache;
+        }
+
+
+        public void Apply()
+        {
+            checkoutDistanceCache = -1; // Force a checkout distance calculation based on project Quality level
+            var checkoutDistance = GetCheckoutDistance();
+            Shader.SetGlobalFloat("_GlobalClipDistance", checkoutDistance);
+            var tiles = FindObjectsOfType<TileEnabler>();
+            foreach (var tile in tiles)
+            {
+                tile.CheckoutDistance = checkoutDistance;
+            }
+        }
+
+#if UNITY_EDITOR
+        // These functions ensure the shader clipping gets reset when the game returns to editor
+        private void OnEnable()
+        {
+            UnityEditor.EditorApplication.playModeStateChanged += PlayModeStateChanged;
+        }
+
+        private void OnDisable()
+        {
+            UnityEditor.EditorApplication.playModeStateChanged -= PlayModeStateChanged;
+            RestoreClipDistance();
+        }
+
+        private void PlayModeStateChanged(UnityEditor.PlayModeStateChange stateChange)
+        {
+            if (stateChange != UnityEditor.PlayModeStateChange.EnteredEditMode)
+            {
+                return;
+            }
+
+            RestoreClipDistance();
+        }
+
+        private void RestoreClipDistance()
+        {
+            Shader.SetGlobalFloat("_GlobalClipDistance", -1);
+            ShowPreview = false;
+        }
+#endif
+    }
+
+    [Serializable]
+    public class MapQualityLevelData
+    {
+        public string QualityName;
+        public float CheckoutDistance;
+    }
+}
