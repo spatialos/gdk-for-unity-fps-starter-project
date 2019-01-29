d2044febe93e27155e6059be590ecd77f7ba971c
jchapmanio <39483705+jchapmanio@users.noreply.github.com>
2019-01-29 17:29:53 +0000

    Random Name Generator (required by KillNotificationTester)

---
+++ workers/unity/Assets/Fps/Scripts/GameLogic/RandomNameGenerator.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public static class RandomNameGenerator
+{
+    public static string GetName()
+    {
+        var prefix = Prefixes[Random.Range(0, Prefixes.Length)];
+
+        var postfix = Postfixes[Random.Range(0, Postfixes.Length)];
+
+        var preLength = prefix.Length;
+        var timeOut = 200;
+        while (postfix.Length + preLength > 15 && timeOut > 0)
+        {
+            postfix = Postfixes[Random.Range(0, Postfixes.Length)];
+            timeOut--;
+        }
+
+        var name = prefix + postfix;
+
+        if (timeOut == 0)
+        {
+            return name.Substring(0, 15);
+        }
+
+        if (name.Length < 13)
+        {
+            name += Random.Range(10, 99).ToString();
+        }
+
+        return name;
+    }
+
+
+    private static readonly string[] Prefixes =
+    {
+        "Angry",
+        "Splendid",
+        "Mega",
+        "Ugly",
+        "Noob",
+        "Dead",
+        "Dr",
+        "Cold",
+        "Fast",
+        "Good",
+        "Spark",
+        "Low",
+        "High",
+        "Twisted"
+    };
+
+    private static readonly string[] Postfixes =
+    {
+        "Fox",
+        "Bear",
+        "Noob",
+        "Ghost",
+        "Kid",
+        "Fragger",
+        "Man",
+        "Woman",
+        "Zombie",
+        "Lion",
+        "Jack",
+        "Karen",
+        "Undead"
+    };
+}
