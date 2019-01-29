6dc3627189b00540238834923b35d0a2bb220eb4
jchapmanio <39483705+jchapmanio@users.noreply.github.com>
2019-01-29 17:35:24 +0000

    Moved controller UI scripts to UI/Controller folder

--- workers/unity/Assets/Fps/Scripts/UI/Controllers/MatchmakingScreenController.cs
+++ workers/unity/Assets/Fps/Scripts/UI/Controllers/MatchmakingScreenController.cs
@@ -7,6 +7,7 @@ public class MatchmakingScreenController : MonoBehaviour
     public InputFieldPlayerName PlayerNameInputField;
     public FpsUIButton QuickJoinButton;
     public FpsUIButton BrowseButton;
+    public FrontEndUIController frontEndUIController;
 
     private const int quickJoinDefaultIndex = 0;
     private const int quickJoinCancelIndex = 1;
@@ -14,6 +15,7 @@ public class MatchmakingScreenController : MonoBehaviour
 
     private void Awake()
     {
+        frontEndUIController = GetComponentInParent<FrontEndUIController>();
         PlayerNameInputField.OnNameChanged += SetButtonsEnabled;
         Debug.Assert(PlayerNameInputField != null);
         Debug.Assert(BrowseButton != null);
@@ -45,5 +47,15 @@ public class MatchmakingScreenController : MonoBehaviour
     {
         QuickJoinButton.SetText(quickJoinDefaultIndex);
         SetButtonsEnabled(true);
+    }
+
+    public void QuickJoinPressed()
+    {
+
+    }
+
+    public void BrowsePressed()
+    {
+        frontEndUIController.SetScreenTo(FrontEndUIController.ScreenType.Browse);
     }
 }
