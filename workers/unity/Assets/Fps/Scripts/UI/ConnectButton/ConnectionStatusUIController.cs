99b04cb5e057f38a6593fb6a36fcc23db516c9b7
jchapmanio <39483705+jchapmanio@users.noreply.github.com>
2019-02-06 15:54:08 +0000

    Renamed methods and vars to match connection status enum

--- workers/unity/Assets/Fps/Scripts/UI/ConnectButton/ConnectionStatusUIController.cs
+++ workers/unity/Assets/Fps/Scripts/UI/ConnectButton/ConnectionStatusUIController.cs
@@ -9,12 +9,13 @@ public class ConnectionStatusUIController : MonoBehaviour
     public GameObject SuccessSymbol;
     public Text StatusText;
 
-    public string GatheringDeploymentsText = "Getting deployment list...";
-    public string GatheringDeploymentsFailedText = "Failed to get deployment list!";
-    public string SearchingText = "Searching for deployments...";
-    public string SearchFailedText = "Failed to find deployments!";
-    public string JoiningText = "Joining deployment...";
-    public string SpawnFailedText = "Failed to spawn player!";
+    public string DeploymentListAvailableText = "Deployments available";
+    public string GettingDeploymentListText = "Getting deployment list...";
+    public string FailedToGetDeploymentListText = "Failed to get deployment list!";
+    public string ConnectingText = "Searching for deployment to join...";
+    public string ConnectionFailedText = "Failed to join deployment!";
+    public string SpawningText = "Joining deployment...";
+    public string SpawningFailedText = "Failed to spawn player!";
     public string WorkerDisconnectedText = "Worker was disconnected";
 
     private void Awake()
@@ -32,90 +33,90 @@ public class ConnectionStatusUIController : MonoBehaviour
         switch (state)
         {
             case ConnectionStateReporter.State.None:
-                Initialize();
+                State_None();
                 break;
             case ConnectionStateReporter.State.GettingDeploymentList:
-                GatheringDeployments();
+                State_GettingDeploymentList();
                 break;
             case ConnectionStateReporter.State.DeploymentListAvailable:
-                DeploymentsAvailable();
+                State_DeploymentListAvailable();
                 break;
             case ConnectionStateReporter.State.FailedToGetDeploymentList:
-                FailedToGetDeployments(information);
+                State_FailedToGetDeploymentList(information);
                 break;
             case ConnectionStateReporter.State.Connecting:
-                SearchForDeployment();
+                State_Connecting();
                 break;
             case ConnectionStateReporter.State.Connected:
                 ConnectionStateReporter.TrySpawn();
                 break;
             case ConnectionStateReporter.State.ConnectionFailed:
-                SearchForDeploymentFailed();
+                State_ConnectionFailed(information);
                 break;
             case ConnectionStateReporter.State.Spawning:
-                Spawning();
+                State_Spawning();
                 break;
             case ConnectionStateReporter.State.Spawned:
                 break;
             case ConnectionStateReporter.State.SpawningFailed:
-                SpawnFailed();
+                State_SpawningFailed(information);
                 break;
             case ConnectionStateReporter.State.WorkerDisconnected:
-                WorkerDisconnected();
+                State_WorkerDisconnected();
                 break;
             default:
                 throw new ArgumentOutOfRangeException(nameof(state), state, null);
         }
     }
 
-    private void Initialize()
+    private void State_None()
     {
         StatusText.text = "";
         SetSymbol(null);
     }
 
-    private void GatheringDeployments()
+    private void State_GettingDeploymentList()
     {
-        StatusText.text = GatheringDeploymentsText;
+        StatusText.text = GettingDeploymentListText;
         SetSymbol(SpinnerSymbol);
     }
 
-    private void DeploymentsAvailable()
+    private void State_DeploymentListAvailable()
     {
-        StatusText.text = "Deployments available";
+        StatusText.text = DeploymentListAvailableText;
         SetSymbol(SuccessSymbol);
     }
 
-    private void FailedToGetDeployments(string error)
+    private void State_FailedToGetDeploymentList(string error)
     {
-        StatusText.text = $"{GatheringDeploymentsFailedText} {error}";
+        StatusText.text = $"{FailedToGetDeploymentListText}\n Error: {error}";
         SetSymbol(ErrorSymbol);
     }
 
-    private void SearchForDeployment()
+    private void State_Connecting()
     {
-        StatusText.text = SearchingText;
+        StatusText.text = ConnectingText;
         SetSymbol(SpinnerSymbol);
     }
 
-    private void SearchForDeploymentFailed()
+    private void State_ConnectionFailed(string error)
     {
-        StatusText.text = SearchFailedText;
+        StatusText.text = $"{ConnectionFailedText}\nError: {error}";
         SetSymbol(ErrorSymbol);
     }
 
-    private void Spawning()
+    private void State_Spawning()
     {
-        StatusText.text = JoiningText;
+        StatusText.text = SpawningText;
     }
 
-    private void SpawnFailed()
+    private void State_SpawningFailed(string error)
     {
-        StatusText.text = SpawnFailedText;
+        StatusText.text = $"{SpawningFailedText}\nError: {error}";
         SetSymbol(ErrorSymbol);
     }
 
-    private void WorkerDisconnected()
+    private void State_WorkerDisconnected()
     {
         StatusText.text = WorkerDisconnectedText;
         SetSymbol(ErrorSymbol);
