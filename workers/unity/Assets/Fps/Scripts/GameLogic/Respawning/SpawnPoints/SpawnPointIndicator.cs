45c50cde84417f80535ee0c03924773d1843fd3f
jchapmanio <39483705+jchapmanio@users.noreply.github.com>
2019-02-18 18:48:02 +0000

    Updated visuals to be more indicative of player size + more visible in scene

--- workers/unity/Assets/Fps/Scripts/GameLogic/Respawning/SpawnPoints/SpawnPointIndicator.cs
+++ workers/unity/Assets/Fps/Scripts/GameLogic/Respawning/SpawnPoints/SpawnPointIndicator.cs
@@ -4,11 +4,12 @@ namespace Fps
 {
     public class SpawnPointIndicator : MonoBehaviour
     {
-        private readonly Vector3 boxDimensions = new Vector3(1, 0, 1);
+        private static readonly Color col = new Color(.3f, 1f, .3f, .3f);
+        public static readonly Vector3 BoxDimensions = new Vector3(.5f, 2f, .5f);
 
         private void OnDrawGizmos()
         {
-            Gizmos.color = Color.blue;
+            Gizmos.color = col;
             var position = transform.position;
             var rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
             Gizmos.matrix = Matrix4x4.TRS(
@@ -17,8 +18,9 @@ namespace Fps
                 Vector3.one
             );
 
-            Gizmos.DrawWireCube(Vector3.zero, boxDimensions);
-            Gizmos.DrawLine(Vector3.zero, Vector3.forward * boxDimensions.z / 2);
+            Gizmos.DrawCube(Vector3.zero, BoxDimensions);
+            Gizmos.DrawLine(Vector3.down * BoxDimensions.y * .5f,
+                Vector3.down * BoxDimensions.y * .5f + Vector3.forward * BoxDimensions.z);
         }
     }
 }
