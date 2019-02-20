cac7505be924e40ef9bb279ace788348827a9334
jchapmanio <39483705+jchapmanio@users.noreply.github.com>
2019-02-20 14:54:01 +0000

    MapBuilder uses texture lookup to place tiles. MaptileCollectionAsset holds list of prefabs to spawn for associated colour found in texture lookup.

--- workers/unity/Assets/Fps/Scripts/MapGeneration/MapBuilder.cs
+++ workers/unity/Assets/Fps/Scripts/MapGeneration/MapBuilder.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Collections.Generic;
+<<<<<<< HEAD
 using System.Linq;
 using Improbable.Gdk.Core;
 using Improbable.Worker.CInterop;
+=======
+>>>>>>> MapBuilder uses texture lookup to place tiles. MaptileCollectionAsset holds list of prefabs to spawn for associated colour found in texture lookup.
 using UnityEngine;
 using Object = UnityEngine.Object;
 using Random = UnityEngine.Random;
 #if UNITY_EDITOR
 using UnityEditor;
+
 #endif
 
 namespace Fps
@@ -23,6 +27,22 @@ namespace Fps
 
         private MapBuilderSettings mapBuilderSettings;
 
+<<<<<<< HEAD
+=======
+        public Texture2D MapTileLookupTexture;
+        public MapTileCollectionAsset DefaultTileCollection;
+        public MapTileCollectionAsset[] ExtraTileCollections;
+
+        // Measurements.
+        // All sizes are 1:1 ratio in X/Z, so we just define one value to represent both axis.
+        public const int UnitsPerBlock = 4; // One textured square on the ground is a 'block'.
+        public const int UnitsPerTile = 9 * UnitsPerBlock;
+        public const int TilesPerGroundLayer = 4; // Ground layers are large quads that encompass 4x4 tiles.
+        public const int BoundaryCollisionHeight = 16;
+
+
+#if UNITY_EDITOR
+>>>>>>> MapBuilder uses texture lookup to place tiles. MaptileCollectionAsset holds list of prefabs to spawn for associated colour found in texture lookup.
         // Store the half-value as many calculations are simplified by going from -halfNumGroundLayers to halfNumGroundLayers.
         private int halfNumGroundLayers => (Layers - 1) / mapBuilderSettings.TilesPerGroundLayer + 1;
 
@@ -84,6 +104,13 @@ namespace Fps
                 Debug.LogError("Generation aborted (See previous message)");
                 return;
             }
+
+            if (DefaultTileCollection == null)
+            {
+                Debug.LogWarning("Must have at least a Default Tile Collection asset reference to build the map");
+                return;
+            }
+
 
             Clean();
 
@@ -388,12 +415,48 @@ namespace Fps
 
         private void PlaceTile(Vector2Int tileCoord)
         {
+            GameObject tile = null;
+        if(MapTileLookupTexture!=null){
+
+            var textureCentre = Vector2Int.one * -1 +
+                new Vector2Int(MapTileLookupTexture.width / 2, MapTileLookupTexture.height / 2);
+            var texturePixelCoord = textureCentre + tileCoord;
+
+            var pixel = MapTileLookupTexture.GetPixel(texturePixelCoord.x, texturePixelCoord.y);
+
+
+            if (pixel == DefaultTileCollection.PixelRepresentationColor)
+            {
+                tile = DefaultTileCollection.GetRandomTile();
+            }
+            else
+            {
+                foreach (var tileCollection in ExtraTileCollections)
+                {
+                    if (pixel == tileCollection.PixelRepresentationColor)
+                    {
+                        tile = tileCollection.GetRandomTile();
+                        break;
+                    }
+                }
+            }
+        }else{
+            tile = DefaultTileCollection.GetRandomTile();
             if (Random.value < EmptyTileChance)
             {
+                tile = null;
+            }
+        }
+
+
+            if (tile == null)
+            {
+                //Debug.Log($"<b><color=#{ColorUtility.ToHtmlStringRGBA(pixel)}>{pixel}</color></b> did not match anything");
                 return;
             }
 
-            var tile = levelTiles[Random.Range(0, levelTiles.Length)];
+
+            //var tile = levelTiles[Random.Range(0, levelTiles.Length)];
             float rotation = 90 * Random.Range(0, 4);
 
             PlaceTile(tileCoord, tile, rotation);
