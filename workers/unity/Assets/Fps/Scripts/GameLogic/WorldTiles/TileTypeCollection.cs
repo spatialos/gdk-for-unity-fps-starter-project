a9ee5970c4fec6b2f98ccbe61d751fb9a43b7389
jchapmanio <39483705+jchapmanio@users.noreply.github.com>
2019-02-27 19:43:16 +0000

    Scripts for tile type collection and volume

---
+++ workers/unity/Assets/Fps/Scripts/GameLogic/WorldTiles/TileTypeCollection.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Fps
+{
+    [CreateAssetMenu(fileName = "New Prefab Collection", menuName = "Improbable/Create Prefab Collection")]
+    public class TileTypeCollection : ScriptableObject
+    {
+        [SerializeField] private GameObject[] Tiles;
+        [Range(0f, 1f)] public float ChanceOfEmptyTile;
+        public Color DisplayColor = Color.blue;
+
+        public GameObject GetRandomTile()
+        {
+            var rand = Random.value;
+            if (ChanceOfEmptyTile == 0f || rand > ChanceOfEmptyTile)
+            {
+                return Tiles[Random.Range(0, Tiles.Length)];
+            }
+
+            return null;
+        }
+    }
+}
