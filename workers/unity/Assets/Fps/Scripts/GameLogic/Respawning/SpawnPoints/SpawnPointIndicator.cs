613ad5f3d8bb02e2ce0ab2e749e68c6cdb7c7712
jchapmanio <39483705+jchapmanio@users.noreply.github.com>
2019-03-07 17:10:04 +0000

    Renamed colour var.

--- workers/unity/Assets/Fps/Scripts/GameLogic/Respawning/SpawnPoints/SpawnPointIndicator.cs
+++ workers/unity/Assets/Fps/Scripts/GameLogic/Respawning/SpawnPoints/SpawnPointIndicator.cs
@@ -4,12 +4,12 @@ namespace Fps
 {
     public class SpawnPointIndicator : MonoBehaviour
     {
-        private static readonly Color transparentLightGreen = new Color(.1f, 1f, .1f, .8f);
+        private static readonly Color gizmoColor = new Color(.1f, 1f, .1f, .8f); // Transparent green
         public static readonly Vector3 BoxDimensions = new Vector3(.5f, 2f, .5f);
 
         private void OnDrawGizmos()
         {
-            Gizmos.color = transparentLightGreen;
+            Gizmos.color = gizmoColor;
             var position = transform.position;
             var rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
             Gizmos.matrix = Matrix4x4.TRS(
