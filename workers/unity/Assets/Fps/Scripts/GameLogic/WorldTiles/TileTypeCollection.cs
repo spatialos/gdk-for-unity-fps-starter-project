be271cb57c90e6f312abdc305bed41580099e088
jchapmanio <39483705+jchapmanio@users.noreply.github.com>
2019-03-07 13:57:54 +0000

    Feature/aaa mapgenerator update (#118)
    * Removed fixed centre tiles from map builder. Added ability to generate 1 of each tile type. Added ability to block tile generation using MapBuilderTileBlocker component. Tiles instantiated by MapBuilder now keep their prefab connection.
    
    * Moved map generation scripts into own folder. Added tile footprint helper component (Used for authoring new tiles)
    
    * Added TileTypeCollection scriptableobject. Added collection objects for various tile types
    
    * Added tile type volume prefab for small and large worlds
    
    * Updated Mapbuilder to use tile type collections specified by volumes
    
    * Updated navmesh
    
    * (MapBuilder) Removed empty tile chance (as now defined per-collection). Refactored method.

---
+++ workers/unity/Assets/Fps/Scripts/GameLogic/WorldTiles/TileTypeCollection.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Fps
+{
+    [CreateAssetMenu(fileName = "New TileTypeCollection", menuName = "Improbable/Tile Type Collection")]
+    public class TileTypeCollection : ScriptableObject
+    {
+        [SerializeField] private GameObject[] tiles;
+        [SerializeField] [Range(0f, 1f)] private float chanceOfEmptyTile;
+        [SerializeField] private Color displayColor = Color.blue;
+        public Color DisplayColor => displayColor;
+
+        public GameObject GetRandomTile()
+        {
+            var rand = Random.value;
+            if (chanceOfEmptyTile == 0f || rand > chanceOfEmptyTile)
+            {
+                return tiles[Random.Range(0, tiles.Length)];
+            }
+
+            return null;
+        }
+    }
+}
---
+++ workers/unity/Assets/Fps/Scripts/GameLogic/WorldTiles/TileTypeVolume.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Fps
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class TileTypeVolume : MonoBehaviour
+    {
+        public TileTypeCollection TypeCollection;
+        public bool AutoRename;
+        private BoxCollider boxCollider;
+
+        private void Awake()
+        {
+            boxCollider = GetComponent<BoxCollider>();
+        }
+
+        private void OnValidate()
+        {
+            if (!AutoRename)
+            {
+                return;
+            }
+
+            gameObject.name = TypeCollection == null ? "Empty TileTypeCollection" : $"{TypeCollection.name} TypeVolume";
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (TypeCollection == null)
+            {
+                return;
+            }
+
+            if (boxCollider == null)
+            {
+                boxCollider = GetComponent<BoxCollider>();
+            }
+
+            Gizmos.color = TypeCollection.DisplayColor;
+            Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
+            Gizmos.DrawCube(boxCollider.center, boxCollider.size);
+        }
+
+        public GameObject GetRandomTile()
+        {
+            return TypeCollection?.GetRandomTile();
+        }
+    }
+}
