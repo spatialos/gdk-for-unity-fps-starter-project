34449a80a58b332435fe017d82bc86c2c0a9923b
jchapmanio <39483705+jchapmanio@users.noreply.github.com>
2019-03-07 17:29:04 +0000

    Made boxDimensions private again.

--- workers/unity/Assets/Fps/Scripts/GameLogic/Respawning/SpawnPoints/SpawnPointIndicator.cs
+++ workers/unity/Assets/Fps/Scripts/GameLogic/Respawning/SpawnPoints/SpawnPointIndicator.cs
@@ -1,16 +1,15 @@
-﻿using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Fps
 {
     public class SpawnPointIndicator : MonoBehaviour
     {
-        private static readonly Color col = new Color(.1f, 1f, .1f, .8f);
-        public static readonly Vector3 BoxDimensions = new Vector3(.5f, 2f, .5f);
+        private static readonly Color gizmoColor = new Color(.1f, 1f, .1f, .8f); // Transparent green
+        private static readonly Vector3 boxDimensions = new Vector3(.5f, 2f, .5f);
 
         private void OnDrawGizmos()
         {
-            Gizmos.color = col;
+            Gizmos.color = gizmoColor;
             var position = transform.position;
             var rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
             Gizmos.matrix = Matrix4x4.TRS(
@@ -19,8 +18,8 @@ namespace Fps
                 Vector3.one
             );
 
-            Gizmos.DrawCube(Vector3.up * BoxDimensions.y * .5f, BoxDimensions);
-            Gizmos.DrawLine(Vector3.forward * BoxDimensions.z * .5f, Vector3.forward * BoxDimensions.z);
+            Gizmos.DrawCube(Vector3.up * boxDimensions.y * .5f, boxDimensions);
+            Gizmos.DrawLine(Vector3.forward * boxDimensions.z * .5f, Vector3.forward * boxDimensions.z);
         }
     }
 }
