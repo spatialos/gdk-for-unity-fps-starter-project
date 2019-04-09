32eb3e26c75add4761a8e41fac0c2ad14988b608
Martijn Gerkes <martijn@improbable.io>
2019-04-09 13:43:59 +0100

    Refactor systems to no longer use reactive components

--- workers/unity/Packages/com.improbable.gdk.health/Systems/ServerHealthModifierSystem.cs
+++ workers/unity/Packages/com.improbable.gdk.health/Systems/ServerHealthModifierSystem.cs
@@ -1,5 +1,4 @@
 using Improbable.Gdk.Core;
-using Improbable.Gdk.Health;
 using Unity.Collections;
 using Unity.Entities;
 using UnityEngine;
@@ -9,54 +8,57 @@ namespace Improbable.Gdk.Health
     [UpdateInGroup(typeof(SpatialOSUpdateGroup))]
     public class ServerHealthModifierSystem : ComponentSystem
     {
-        private struct EntitiesWithModifiedHealth
-        {
-            public readonly int Length;
-            [ReadOnly] public EntityArray Entity;
-            public ComponentDataArray<HealthComponent.Component> Health;
-            [ReadOnly] public ComponentDataArray<HealthComponent.CommandRequests.ModifyHealth> ModifyHealthRequests;
-            public ComponentDataArray<HealthComponent.EventSender.HealthModified> HealthModifiedEventSenders;
-        }
+        private WorkerSystem workerSystem;
+        private CommandSystem commandSystem;
+        private ComponentUpdateSystem componentUpdateSystem;
 
-        [Inject] private EntitiesWithModifiedHealth entitiesWithModifiedHealth;
+        protected override void OnCreateManager()
+        {
+            base.OnCreateManager();
+
+            workerSystem = World.GetExistingManager<WorkerSystem>();
+            commandSystem = World.GetExistingManager<CommandSystem>();
+            componentUpdateSystem = World.GetExistingManager<ComponentUpdateSystem>();
+        }
 
         protected override void OnUpdate()
         {
-            for (var i = 0; i < entitiesWithModifiedHealth.Length; i++)
+            var requests = commandSystem.GetRequests<HealthComponent.ModifyHealth.ReceivedRequest>();
+            var healthComponentData = GetComponentDataFromEntity<HealthComponent.Component>();
+            for (var i = 0; i < requests.Count; i++)
             {
-                var health = entitiesWithModifiedHealth.Health[i];
-                var healthModifiedEventSender = entitiesWithModifiedHealth.HealthModifiedEventSenders[i];
+                ref readonly var request = ref requests[i];
+                var entityId = request.EntityId;
+                if (!workerSystem.TryGetEntity(entityId, out var entity))
+                {
+                    continue;
+                }
 
+                var health = healthComponentData[entity];
+
+                // Skip if already dead
                 if (health.Health <= 0)
                 {
                     continue;
                 }
 
-                foreach (var request in entitiesWithModifiedHealth.ModifyHealthRequests[i].Requests)
+                var modifier = request.Payload;
+                var healthModifiedInfo = new HealthModifiedInfo
                 {
-                    var modifier = request.Payload;
+                    Modifier = modifier,
+                    HealthBefore = health.Health
+                };
 
-                    var healthModifiedInfo = new HealthModifiedInfo
-                    {
-                        Modifier = modifier,
-                        HealthBefore = health.Health
-                    };
+                health.Health = Mathf.Clamp(health.Health + modifier.Amount, 0, health.MaxHealth);
+                healthModifiedInfo.HealthAfter = health.Health;
 
-                    health.Health = Mathf.Clamp(health.Health + modifier.Amount, 0, health.MaxHealth);
-                    healthModifiedInfo.HealthAfter = health.Health;
-
-
-                    if (health.Health <= 0)
-                    {
-                        healthModifiedInfo.Died = true;
-                        healthModifiedEventSender.Events.Add(healthModifiedInfo);
-                        break;
-                    }
-
-                    healthModifiedEventSender.Events.Add(healthModifiedInfo);
+                if (health.Health <= 0)
+                {
+                    healthModifiedInfo.Died = true;
                 }
 
-                entitiesWithModifiedHealth.Health[i] = health;
+                componentUpdateSystem.SendEvent(new HealthComponent.HealthModified.Event(healthModifiedInfo), entityId);
+                healthComponentData[entity] = health;
             }
         }
     }
