8fd22457fc4ed4142d1e9db003f5f57dd6c1dabe
Martijn Gerkes <martijn@improbable.io>
2019-04-10 12:11:40 +0100

    Remove logic from TileEnabler Move environment rendering to separate skybox camera Environment cloud and terrain now a combined prefab for all worker scenes Set player camera far clipping plane to checkout distance + bias Player cameras now seperate prefab for easier tweaking

---
+++ workers/unity/Assets/Fps/Scripts/GameLogic/SkyboxCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class SkyboxCamera : MonoBehaviour
+{
+    private Camera myCamera;
+
+    private void Awake()
+    {
+        myCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        var playerCamera = Camera.main;
+        if (playerCamera == null)
+        {
+            return;
+        }
+
+        myCamera.transform.rotation = playerCamera.transform.rotation;
+        myCamera.fieldOfView = playerCamera.fieldOfView;
+    }
+}
---
+++ workers/unity/Assets/Fps/Scripts/SetupLogic/CameraClipQuality.cs
@@ -0,0 +1,13 @@
+﻿using Fps;
+using UnityEngine;
+
+public class CameraClipQuality : MonoBehaviour
+{
+    [SerializeField] private float checkoutBias = 10.0f;
+
+    void Awake()
+    {
+        var camera = GetComponent<Camera>();
+        camera.farClipPlane = MapQualitySettings.CheckoutDistance + checkoutBias;
+    }
+}
