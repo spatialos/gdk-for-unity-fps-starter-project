88c8117681c440da85aa0e2465fc856d84e52e39
Jessica Falk <jessica@improbable.io>
2019-04-11 16:13:28 +0100

    ConnectButton controller scripts

---
+++ workers/unity/Assets/Fps/Scripts/UI/ConnectButton/ConnectionStatusUIController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace Fps
+{
+    public class ConnectionStatusUIController : MonoBehaviour
+    {
+        public GameObject SpinnerSymbol;
+        public GameObject ErrorSymbol;
+        public GameObject SuccessSymbol;
+        public Text StatusText;
+
+        private const string DeploymentListAvailableText = "Deployments available";
+        private const string GettingDeploymentListText = "Getting deployment list...";
+        private const string FailedToGetDeploymentListText = "Failed to get deployment list!";
+        private const string ConnectionFailedText = "Failed to join deployment!";
+        private const string ConnectedText = "Joined deployment!";
+        private const string WaitingForGameStartText = "Waiting for game to start...";
+        private const string SpawningText = "Spawning player...";
+        private const string GameReadyText = "Press start to play";
+        private const string ConnectingText = "Joining deployment...";
+        private const string SpawningFailedText = "Failed to spawn player!";
+        private const string WorkerDisconnectedText = "Worker was disconnected!";
+
+        public void ShowGetDeploymentListText()
+        {
+            StatusText.text = GettingDeploymentListText;
+            SetSymbol(SpinnerSymbol);
+        }
+
+        public void ShowDeploymentListAvailableText()
+        {
+            StatusText.text = DeploymentListAvailableText;
+            SetSymbol(SuccessSymbol);
+        }
+
+        public void ShowFailedToGetDeploymentsText(string error)
+        {
+            StatusText.text = $"{FailedToGetDeploymentListText}\n Error: {error}";
+            SetSymbol(ErrorSymbol);
+        }
+
+        public void ShowConnectingText()
+        {
+            StatusText.text = ConnectingText;
+            SetSymbol(SpinnerSymbol);
+        }
+
+        public void ShowConnectionFailedText(string error)
+        {
+            StatusText.text = $"{ConnectionFailedText}\nError: {error}";
+            SetSymbol(ErrorSymbol);
+        }
+
+        public void ShowWaitForGameText()
+        {
+            StatusText.text = WaitingForGameStartText;
+            SetSymbol(SpinnerSymbol);
+        }
+
+        public void ShowGameReadyText()
+        {
+            SetSymbol(SuccessSymbol);
+            StatusText.text = GameReadyText;
+        }
+
+        public void ShowSpawningText()
+        {
+            StatusText.text = SpawningText;
+        }
+
+        public void ShowSpawningFailedText(string error)
+        {
+            StatusText.text = $"{SpawningFailedText}\nError: {error}";
+            SetSymbol(ErrorSymbol);
+        }
+
+        public void ShowWorkerDisconnectedText()
+        {
+            StatusText.text = WorkerDisconnectedText;
+            SetSymbol(ErrorSymbol);
+        }
+
+        private void SetSymbol(GameObject symbol)
+        {
+            Debug.Assert(symbol == null
+                || symbol == ErrorSymbol
+                || symbol == SpinnerSymbol
+                || symbol == SuccessSymbol);
+
+            if (ErrorSymbol != null)
+            {
+                ErrorSymbol.SetActive(symbol == ErrorSymbol);
+            }
+
+            if (SpinnerSymbol != null)
+            {
+                SpinnerSymbol.SetActive(symbol == SpinnerSymbol);
+            }
+
+            if (SuccessSymbol != null)
+            {
+                SuccessSymbol.SetActive(symbol == SuccessSymbol);
+            }
+        }
+
+        private void OnDisable()
+        {
+            StopAllCoroutines();
+        }
+    }
+}
