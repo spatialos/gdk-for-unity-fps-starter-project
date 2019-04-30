9f46c663b7bf3787c39421f279a0388c215b1f88
Jamie Brynes <jamiebrynes@improbable.io>
2019-04-30 13:15:54 +0100

    Fix BuildKite (#167)

--- ci/bootstrap.sh
+++ ci/bootstrap.sh
@@ -8,12 +8,11 @@ CLONE_URL="git@github.com:spatialos/gdk-for-unity-shared-ci.git"
 
 # Clone the HEAD of the shared CI repo into ".shared-ci"
 
-if [ -d "${SHARED_CI_DIR}" ]; then
+if [[ -d "${SHARED_CI_DIR}" ]]; then
     rm -rf "${SHARED_CI_DIR}"
 fi
 
-git clone "${CLONE_URL}" "${SHARED_CI_DIR}"
-
+git clone --verbose --depth 1 "${CLONE_URL}" "${SHARED_CI_DIR}"
 
 # Clone the GDK for Unity repository
 
@@ -23,7 +22,12 @@ PINNED_VERSION=$(cat ./gdk.pinned)
 
 rm -rf "${TARGET_DIRECTORY}"
 
-git clone ${CLONE_URI} "${TARGET_DIRECTORY}"
+mkdir "${TARGET_DIRECTORY}"
+
+# Workaround for being unable to clone a specific commit with depth of 1.
 pushd "${TARGET_DIRECTORY}"
+    git init
+    git remote add origin "${CLONE_URI}"
+    git fetch --depth 20 origin develop
     git checkout "${PINNED_VERSION}"
 popd
