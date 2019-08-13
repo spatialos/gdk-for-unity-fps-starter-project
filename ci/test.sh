2c8e96b795f0667e9f06f05d39f47305ead7669c
Jamie Brynes <jamiebrynes@improbable.io>
2019-08-13 12:04:33 +0100

    add to buildkite

---
+++ ci/test.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e -u -o pipefail
+
+if [[ -n "${DEBUG-}" ]]; then
+  set -x
+fi
+
+cd "$(dirname "$0")/../"
+
+source .shared-ci/scripts/pinned-tools.sh
+
+PROJECT_DIR="$(pwd)"
+mkdir -p "${PROJECT_DIR}/logs/"
+
+EDITMODE_TEST_RESULTS_FILE="${PROJECT_DIR}/logs/editmode-test-results.xml"
+
+echo "--- Testing Unity: Editmode :writing_hand:"
+
+pushd "workers/unity"
+    dotnet run -p "${PROJECT_DIR}/.shared-ci/tools/RunUnity/RunUnity.csproj" -- \
+        -batchmode \
+        -projectPath "${PROJECT_DIR}/workers/unity" \
+        -runTests \
+        -testPlatform editmode \
+        -logfile "${PROJECT_DIR}/logs/unity-editmode-test-run.log" \
+        -testResults "${EDITMODE_TEST_RESULTS_FILE}" \
+        -editorTestsFilter Fps
+popd
