de030bd2456e7dd06a4ab5257815affd637a8b29
Paul Balaji <paulbalaji@improbable.io>
2020-06-08 15:13:48 +0100

    New Buildkite instrumentation format (#276)

--- ci/bootstrap.sh
+++ ci/bootstrap.sh
@@ -2,33 +2,34 @@
 set -e -u -o pipefail
 
 if [[ -n "${DEBUG-}" ]]; then
-  set -x
+    set -x
 fi
 
 function isLinux() {
-  [[ "$(uname -s)" == "Linux" ]];
+    [[ "$(uname -s)" == "Linux" ]];
 }
 
 function isMacOS() {
-  [[ "$(uname -s)" == "Darwin" ]];
+    [[ "$(uname -s)" == "Darwin" ]];
 }
 
 function isWindows() {
-  ! ( isLinux || isMacOS );
+    ! ( isLinux || isMacOS );
 }
 
 cd "$(dirname "$0")/../"
 
-echo "--- Bootstrapping :boot:"
+echo "## imp-ci group-start Bootstrapping :boot:"
 
 ./ci/get-shared-ci.sh
+source ".shared-ci/scripts/pinned-tools.sh"
 
 # Clone the GDK for Unity repository
 
 CLONE_URI="git@github.com:spatialos/gdk-for-unity.git"
 pushd ../
     TARGET_DIRECTORY="$(pwd)/gdk-for-unity"
-popd 
+popd
 PINNED_BRANCH=$(cat ./gdk.pinned | cut -d' ' -f 1)
 PINNED_VERSION=$(cat ./gdk.pinned | cut -d' ' -f 2)
 SKIP_GDK=false
@@ -58,30 +59,36 @@ if [ "$SKIP_GDK" = false ] ; then
         git remote add origin "${CLONE_URI}"
         git fetch --depth 20 origin "${PINNED_BRANCH}"
         git checkout "${PINNED_VERSION}"
-        echo "--- Hit init :right-facing_fist::red_button:"
-        ./init.sh $@
+
+        traceStart "Hit init :right-facing_fist::red_button:"
+            ./init.sh $@
+        traceEnd
     popd
 fi
 
-echo "--- Symlinking packages :link::package:"
-
-if isWindows; then
-    if [[ -z ${BUILDKITE:-} ]]; then
-        powershell Start-Process -Verb RunAs -WindowStyle Hidden -Wait -FilePath dotnet.exe -ArgumentList "run", "-p", "$(pwd)/.shared-ci/tools/PackageSymLinker/PackageSymLinker.csproj", "\-\-", "--packages-source-dir", "${TARGET_DIRECTORY}/workers/unity/Packages", "--package-target-dir", "$(pwd)/workers/unity/Packages"
-        exit 0
+traceStart "Symlinking packages :link::package:"
+    if isWindows; then
+        if [[ -z ${BUILDKITE:-} ]]; then
+            powershell Start-Process -Verb RunAs -WindowStyle Hidden -Wait -FilePath dotnet.exe -ArgumentList "run", "-p", "$(pwd)/.shared-ci/tools/PackageSymLinker/PackageSymLinker.csproj", "\-\-", "--packages-source-dir", "${TARGET_DIRECTORY}/workers/unity/Packages", "--package-target-dir", "$(pwd)/workers/unity/Packages"
+            traceEnd
+            echo "## imp-ci group-end Bootstrapping :boot:"
+            exit 0
+        fi
     fi
-fi
 
-EXTRA_ARGS=""
+    EXTRA_ARGS=""
 
-if [[ -n ${BUILDKITE:-} ]]; then
-    EXTRA_ARGS="--copy"
-fi
+    if [[ -n ${BUILDKITE:-} ]]; then
+        EXTRA_ARGS="--copy"
+    fi
 
-if [[ "${BUILDKITE_AGENT_META_DATA_OS:-}" == "darwin" ]]; then
-    PATH="${PATH}:/usr/local/share/dotnet"
-fi
+    if [[ "${BUILDKITE_AGENT_META_DATA_OS:-}" == "darwin" ]]; then
+        PATH="${PATH}:/usr/local/share/dotnet"
+    fi
 
-dotnet run -p ./.shared-ci/tools/PackageSymLinker/PackageSymLinker.csproj -- \
-    --packages-source-dir "${TARGET_DIRECTORY}/workers/unity/Packages" \
-    --package-target-dir "$(pwd)/workers/unity/Packages" "${EXTRA_ARGS}"
+    dotnet run -p ./.shared-ci/tools/PackageSymLinker/PackageSymLinker.csproj -- \
+        --packages-source-dir "${TARGET_DIRECTORY}/workers/unity/Packages" \
+        --package-target-dir "$(pwd)/workers/unity/Packages" "${EXTRA_ARGS}"
+traceEnd
+
+echo "## imp-ci group-end Bootstrapping :boot:"
--- ci/test.sh
+++ ci/test.sh
@@ -3,7 +3,7 @@
 set -e -u -o pipefail
 
 if [[ -n "${DEBUG-}" ]]; then
-  set -x
+    set -x
 fi
 
 cd "$(dirname "$0")/../"
@@ -17,15 +17,15 @@ mkdir -p "${PROJECT_DIR}/logs/"
 
 EDITMODE_TEST_RESULTS_FILE="${PROJECT_DIR}/logs/nunit/editmode-test-results.xml"
 
-echo "--- Testing Unity: Editmode :writing_hand:"
-
-pushd "workers/unity"
-    dotnet run -p "${PROJECT_DIR}/.shared-ci/tools/RunUnity/RunUnity.csproj" -- \
-        -batchmode \
-        -projectPath "${PROJECT_DIR}/workers/unity" \
-        -runEditorTests \
-        -logfile "${PROJECT_DIR}/logs/unity-editmode-test-run.log" \
-        -editorTestsResultFile "${EDITMODE_TEST_RESULTS_FILE}" \
-        -editorTestsFilter Fps \
-        "${ACCELERATOR_ARGS}"
-popd
+traceStart "Testing Unity: Editmode :writing_hand:"
+    pushd "workers/unity"
+        dotnet run -p "${PROJECT_DIR}/.shared-ci/tools/RunUnity/RunUnity.csproj" -- \
+            -batchmode \
+            -projectPath "${PROJECT_DIR}/workers/unity" \
+            -runEditorTests \
+            -logfile "${PROJECT_DIR}/logs/unity-editmode-test-run.log" \
+            -editorTestsResultFile "${EDITMODE_TEST_RESULTS_FILE}" \
+            -editorTestsFilter Fps \
+            "${ACCELERATOR_ARGS}"
+    popd
+traceEnd
